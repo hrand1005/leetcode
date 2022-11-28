531dc41495652594eef4c2790b4f2e5d978ca0ae
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-28 18:41:21 -0500

    Time: 0 ms (100.00%), Space: 2.1 MB (28.73%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.go
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.go
@@ -6,6 +6,8 @@
  *     Right *TreeNode
  * }
  */
+
+/*
 func inorderTraversal(root *TreeNode) []int {
     if root == nil {
         return nil
@@ -13,4 +15,38 @@ func inorderTraversal(root *TreeNode) []int {
     
     left := append(inorderTraversal(root.Left), root.Val)
     return append(left, inorderTraversal(root.Right)...)
+}
+*/
+
+type VisitedNode struct {
+    Node *TreeNode
+    Visited bool
+}
+
+func inorderTraversal(root *TreeNode) []int {
+    inorderPath := []int{}
+    
+    stack := []*VisitedNode{
+        &VisitedNode{
+            Node: root,
+            Visited: false,
+        },
+    }
+    
+    for len(stack) > 0 {
+        this := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        
+        if this.Node != nil {
+            if this.Visited {
+                inorderPath = append(inorderPath, this.Node.Val)
+            } else {
+                stack = append(stack, &VisitedNode{this.Node.Right, false})
+                stack = append(stack, &VisitedNode{this.Node, true})
+                stack = append(stack, &VisitedNode{this.Node.Left, false})
+            }
+        }
+    }
+    
+    return inorderPath
 }
