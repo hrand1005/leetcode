315d33e7e5bd61ce71854fad5573b881935d9f98
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-28 18:09:11 -0500

    Time: 68 ms (10.22%), Space: 13.9 MB (13.85%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -4,10 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root == None:
             return []
         
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
-        
+    
+"""    
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        inorder = []
+        stack = [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node:
+                if visited:
+                    inorder.append(node.val)
+                else:    
+                    stack.append((node.right, False))
+                    stack.append((node, True))
+                    stack.append((node.left, False))
+
+        return inorder
