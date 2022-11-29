749c044fe87746d116b9e66dfab9fc95ccae33db
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-28 19:31:54 -0500

    Time: 5 ms (35.66%), Space: 2.9 MB (22.63%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.go
+++ 0101-symmetric-tree/0101-symmetric-tree.go
@@ -6,6 +6,8 @@
  *     Right *TreeNode
  * }
  */
+
+/*
 func checkMirrored(n1, n2 *TreeNode) bool {
     if n1 == nil && n2 == nil {
         return true
@@ -28,4 +30,35 @@ func isSymmetric(root *TreeNode) bool {
     }
     
     return checkMirrored(root.Left, root.Right)
+}
+*/
+
+
+func isSymmetric(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+    
+    stack := []*TreeNode{root.Left, root.Right}
+    
+    for len(stack) > 0 {
+        n1, n2 := stack[len(stack)-1], stack[len(stack)-2]
+        stack = stack[:len(stack)-2] // 'pop' stack elements
+        
+        if n1 == nil && n2 == nil {
+            continue
+        }
+        
+        if n1 == nil || n2 == nil {
+            return false
+        }
+        
+        if n1.Val != n2.Val {
+            return false
+        }
+        
+        stack = append(stack, n1.Left, n2.Right, n1.Right, n2.Left) 
+    }
+    
+    return true
 }
