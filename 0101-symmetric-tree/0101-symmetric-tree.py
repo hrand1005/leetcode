58ad9b3dea1b13865430f8638dedaf532e2c9daf
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-28 19:15:35 -0500

    Time: 34 ms (94.89%), Space: 14 MB (61.25%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,15 +1,3 @@
-def checkMirrored(node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
-    if node1 == None and node2 == None:
-        return True
-    
-    if node1 == None or node2 == None:
-        return False
-    
-    if node1.val != node2.val:
-        return False
-    
-    return checkMirrored(node1.left, node2.right) and checkMirrored(node1.right, node2.left)
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -18,4 +6,16 @@ def checkMirrored(node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return checkMirrored(root.left, root.right)
+        return self.checkMirrored(root.left, root.right)
+    
+    def checkMirrored(self, node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
+        if node1 == None and node2 == None:
+            return True
+
+        if node1 == None or node2 == None:
+            return False
+
+        if node1.val != node2.val:
+            return False
+
+        return self.checkMirrored(node1.left, node2.right) and self.checkMirrored(node1.right, node2.left)
