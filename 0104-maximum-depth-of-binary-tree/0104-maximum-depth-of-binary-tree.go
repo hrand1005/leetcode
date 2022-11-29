ba475fde596e072b89994fdabca2d5168482e823
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-28 20:36:22 -0500

    Time: 8 ms (49.04%), Space: 4.4 MB (5.02%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.go
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func maxInt(ints []int) int {
+    max := 0
+    for _, v := range ints {
+        if v > max {
+            max = v
+        }
+    }
+    return max
+}
+
+type DepthNode struct {
+    Node *TreeNode
+    Depth int
+}
+
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    
+    pathLengths := []int{}
+    stack := []*DepthNode{
+        {
+            Node: root,
+            Depth: 1,
+        },
+    }
+    
+    for len(stack) > 0 {
+        this := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        
+        if this.Node.Left == nil && this.Node.Right == nil {
+            pathLengths = append(pathLengths, this.Depth)
+            continue
+        }
+        
+        if this.Node.Left != nil {
+            left := &DepthNode{
+                Node: this.Node.Left, 
+                Depth: this.Depth+1,
+            }
+            stack = append(stack,  left)
+        }
+        
+        if this.Node.Right != nil {
+            right := &DepthNode{
+                Node: this.Node.Right, 
+                Depth: this.Depth+1,
+            }
+            stack = append(stack,  right)
+        }
+    }
+    
+    return maxInt(pathLengths)
+}
