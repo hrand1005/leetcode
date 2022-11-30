22b23cf3b2dec25b977b1cd087ee7b89d2f7d5a2
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-30 15:57:19 -0500

    Time: 285 ms (62.77%), Space: 16.5 MB (93.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        occurs = {}
+        for n in nums:
+            if occurs.get(n) == None:
+                occurs[n] = 1
+            else:   
+                occurs[n] += 1
+        
+        for k, v in occurs.items():
+            if v == 1:
+                return k
+            
+        return None
