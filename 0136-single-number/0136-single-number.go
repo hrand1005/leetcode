877a760f66e6ea21c1a5afb80c2096e58e7656e4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-30 16:12:36 -0500

    Time: 47 ms (17.99%), Space: 7.4 MB (8.50%) - LeetHub

---
+++ 0136-single-number/0136-single-number.go
@@ -0,0 +1,12 @@
+func singleNumber(nums []int) int {
+    occ := map[int]int{}
+    for _, n := range nums {
+        occ[n] += 1
+    }
+    for k, v := range occ {
+        if v == 1 {
+            return k
+        }
+    }
+    return 0
+}
