f649b80dfa4a16e2cb26fac9ca764406ddcd97c4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-30 15:48:49 -0500

    Time: 2 ms (87.11%), Space: 2.5 MB (95.15%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -0,0 +1,28 @@
+import (
+    "unicode"
+)
+
+func isPalindrome(s string) bool {
+    i := 0
+    j := len(s) - 1
+    
+    for i < j {
+        left := rune(s[i])
+        right := rune(s[j])
+        
+        if !unicode.IsLetter(left) && !unicode.IsNumber(left) {
+            i++
+            continue
+        }
+        if !unicode.IsLetter(right) && !unicode.IsNumber(right) {
+            j--
+            continue
+        }
+        if unicode.ToLower(left) != unicode.ToLower(right) {
+            return false
+        }
+        
+        i++; j--
+    }
+    return true
+}
