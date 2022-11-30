0127f0fe40260862236d280357508673725346ae
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-11-29 23:30:33 -0500

    Time: 54 ms (57.93%), Space: 13.9 MB (66.91%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         above = [1]
@@ -14,4 +15,12 @@ class Solution:
             above = new_row
             
         return result
-        
+"""
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = [[1]*n for n in range(1, numRows+1)]
+        for i in range(1, numRows):
+            for j in range(1, i):
+                result[i][j] = result[i-1][j-1] + result[i-1][j]
+                
+        return result
