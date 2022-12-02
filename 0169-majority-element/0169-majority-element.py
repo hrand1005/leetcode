2ad07c56bec250bc07f69c94938cd258ef3abf05
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-01 20:38:05 -0500

    Time: 394 ms (46.72%), Space: 15.5 MB (35.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,12 +4,16 @@ class Solution:
             return nums[0]
         
         majority = len(nums) // 2
+        if len(nums) % 2 == 1:
+            majority += 1
+            
         occurrences = {}
         
         for i in range(len(nums)):
             if occurrences.get(nums[i]) == None:
                 occurrences[nums[i]] = 1
-            elif occurrences[nums[i]] >= majority:
-                return nums[i]
             else:
                 occurrences[nums[i]] += 1
+            
+            if occurrences[nums[i]] >= majority:
+                return nums[i]
