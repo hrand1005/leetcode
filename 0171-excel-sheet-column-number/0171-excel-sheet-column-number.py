3e9a551c2d6d5cf7b46ecd5db0c9dad53acf8f0d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-03 13:54:39 -0500

    Time: 71 ms (22.31%), Space: 13.9 MB (56.00%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,39 @@
+POS_MAP = { 
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'H': 8,
+    'I': 9,
+    'J': 10,
+    'K': 11,
+    'L': 12,
+    'M': 13,
+    'N': 14,
+    'O': 15,
+    'P': 16,
+    'Q': 17,
+    'R': 18,
+    'S': 19,
+    'T': 20,
+    'U': 21,
+    'V': 22,
+    'W': 23,
+    'X': 24,
+    'Y': 25,
+    'Z': 26,
+}
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        return self.recursive(0, columnTitle)
+    
+    def recursive(self, multiplier, columnTitle) -> int:
+        if len(columnTitle) == 0:
+            return 0
+        
+        this_val = POS_MAP[columnTitle[len(columnTitle)-1]]
+        return 26 ** multiplier * this_val + self.recursive(multiplier + 1, columnTitle[:-1])
