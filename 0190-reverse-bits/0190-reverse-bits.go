09e3e0663ed01ca1961274b5092aa67728f6b296
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-03 15:05:20 -0500

    Time: 4 ms (45.19%), Space: 2.6 MB (74.04%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.go
@@ -0,0 +1,11 @@
+func reverseBits(num uint32) uint32 {
+    res := uint32(0)
+    
+    for i := 0; i < 32; i++ {
+        res = res << 1
+        res += num % 2
+        num = num >> 1
+    }
+    
+    return res
+}
