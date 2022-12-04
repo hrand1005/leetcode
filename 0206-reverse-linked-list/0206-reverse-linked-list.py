9773d0840726e055cdc88615f417f78f45d55b2d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-04 15:38:15 -0500

    Time: 93 ms (5.34%), Space: 20.4 MB (14.95%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head == None or head.next == None:
@@ -21,4 +22,24 @@ class Solution:
         cur.next = prev     
         
         return cur
+""" 
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+        
+        return self.reverse(None, head)
     
+    def reverse(self, prev, cur):
+        if cur.next == None:
+            cur.next = prev
+            return cur
+        
+        nxt = cur.next
+        cur.next = prev
+        return self.reverse(cur, nxt)
+        
+        
+            
+        
