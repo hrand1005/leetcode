a047d5f3c34151253d58e774f29d526725d8feff
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-04 14:32:13 -0500

    Time: 73 ms (26.59%), Space: 14 MB (15.81%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        occurred = {}
+        
+        while occurred.get(n) == None:
+            occurred[n] = True
+            num_string = str(n)
+            this_sum = 0
+            for digit in num_string:
+                this_sum += int(digit) ** 2
+        
+            if this_sum == 1:
+                return True
+            
+            n = this_sum
+        
+        return False
