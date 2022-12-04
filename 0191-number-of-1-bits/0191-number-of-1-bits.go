b18d0d142924e6cf2393b0739b0349ad095cc3cb
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-03 23:13:17 -0500

    Time: 6 ms (9.70%), Space: 1.9 MB (45.85%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.go
@@ -0,0 +1,15 @@
+func hammingWeight(num uint32) int {
+    if num == 0 {
+        return 0
+    } 
+    
+    count := 0
+    for num > 1 {
+        if num % 2 == 1 {
+            count++
+        }
+        num >>= 1
+    }
+    
+    return count + 1
+}
