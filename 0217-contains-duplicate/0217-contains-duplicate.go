034bb191f854c6c0b1a15ce20f7d9dc15a134cbb
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-05 17:04:56 -0500

    Time: 171 ms (23.34%), Space: 8.4 MB (89.44%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,10 @@
+func containsDuplicate(nums []int) bool {
+    count := map[int]int{}
+    for _, v := range nums {
+        if _, ok := count[v]; ok {
+            return true
+        }
+        count[v]++
+    }
+    return false
+}
