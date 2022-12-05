193b127a7d83ef98aafb4dc9bfc00ccb2133138d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-05 17:41:20 -0500

    Time: 1855 ms (46.72%), Space: 31.1 MB (98.70%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         candidate = []
@@ -16,4 +17,35 @@ class Solution:
             if candidate[i] != candidate[len(candidate)-1-i]:
                 return False
             
-        return True
+        return True    
+"""
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        prev = None
+        slow = head
+        fast = slow
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            
+            slow_next = slow.next
+            slow.next = prev
+            prev = slow
+            slow = slow_next
+        
+        if fast:
+            slow = slow.next
+            
+        first_half = prev
+        second_half = slow
+        
+        while first_half and second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+            
+        return True    
+        
+            
