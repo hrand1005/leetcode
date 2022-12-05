521af434d5d6ba4ea6bb00e63b5850c2b2cf25c2
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-05 17:01:35 -0500

    Time: 1235 ms (22.03%), Space: 26 MB (28.40%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,4 +1,19 @@
+
 """
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+        
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+            
+        return False    
+"""
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         occurred = {}
@@ -9,8 +24,3 @@ class Solution:
             occurred[n] = True
         
         return False
-"""
-
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
