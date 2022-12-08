c3aceef357a0b5cb999b4757a1ae2cb9e497f589
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-07 22:57:18 -0500

    Time: 42 ms (79.87%), Space: 13.8 MB (97.50%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,22 +3,20 @@ class Solution:
         max_val = 2 ** 31 - 1
         min_val = -1 * (2 ** 31)
         
-        result = 0
         multiplier = 1
-        
         if x < 0:
             x *= -1
             multiplier = -1
         
+        result = 0
         while x > 0:
             result = (result * 10) + (x % 10)
             if result > max_val or result < min_val:
                 return 0
             
             x -= x % 10
-            
-            if x == 0:
-                return int(result) * multiplier
+           # if x == 0:
+           #     return int(result) * multiplier
             
             x /= 10
         
