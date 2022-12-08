b3de43fa2f813131d4fdf28daf71748dd1b283f4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-07 23:09:03 -0500

    Time: 5 ms (27.70%), Space: 2.1 MB (27.86%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.go
+++ 0007-reverse-integer/0007-reverse-integer.go
@@ -12,13 +12,14 @@ func reverse(x int) int {
     result := 0
     for x > 0 {
         result = (result * 10) + (x % 10)
-        if result < math.MinInt32 || math.MaxInt32 < result {
-            return 0
-        }
-        
         x -= x % 10
         x /= 10
     }
     
-    return result * multiplier
+    result *= multiplier
+    if result < math.MinInt32 || math.MaxInt32 < result {
+        return 0
+    }
+    
+    return result 
 }
