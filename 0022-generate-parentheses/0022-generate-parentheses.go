159ab9fd38be8961c3c21a1e16ee4e0252bdf000
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-10 18:26:44 -0500

    Time: 3 ms (66.21%), Space: 3 MB (31.21%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.go
+++ 0022-generate-parentheses/0022-generate-parentheses.go
@@ -1,3 +1,4 @@
+/*
 func generateParenthesis(n int) []string {
     return generate(n, n, "", []string{})
 }
@@ -22,4 +23,56 @@ func generate(left, right int, current string, all []string) []string {
     }
     
     return append(lResult, rResult...)
+}
+*/
+
+type parenCombo struct {
+    str string
+    left int
+    right int
+}
+
+func generateParenthesis(n int) []string {
+    allCombos := []string{}
+    
+    stack := []*parenCombo{
+        {
+            str: "(",
+            left: n - 1,
+            right: n,
+        },
+    }
+    
+    for len(stack) > 0 {
+        popped := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        
+        if popped.left > popped.right {
+            continue
+        }
+        
+        if popped.left == 0 && popped.right == 0 {
+            allCombos = append(allCombos, popped.str)
+        }
+        
+        if popped.left > 0 {
+            newCombo := &parenCombo{
+                str: popped.str + "(",
+                left: popped.left-1,
+                right: popped.right,
+            }
+            stack = append(stack, newCombo)
+        }
+        
+        if popped.right > 0 {
+            newCombo := &parenCombo{
+                str: popped.str + ")",
+                left: popped.left,
+                right: popped.right-1,
+            }
+            stack = append(stack, newCombo)
+        }
+    }
+    
+    return allCombos
 }
