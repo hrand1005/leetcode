70013915a2359ce1f090162fa3ab7aefc3815205
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-09 20:19:23 -0500

    Time: 3 ms (38.83%), Space: 2.2 MB (16.06%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,36 @@
+import (
+    "strings"
+)
+
+func letterCombinations(digits string) []string {
+    if len(digits) == 0 {
+        return nil
+    }
+    
+    numLetter := map[string]string{
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+    }
+    
+    combos := map[int][]string{}
+    firstKey := string(digits[0])
+    combos[0] = strings.Split(numLetter[firstKey], "")
+    
+    for i := 1; i < len(digits); i++ {
+        key := string(digits[i])
+        for _, prefix := range combos[i-1] {
+            newLetters := strings.Split(numLetter[key], "")
+            for _, letter := range newLetters {
+                combos[i] = append(combos[i], prefix + string(letter))
+            }
+        }
+    }
+    
+    return combos[len(digits)-1]
+}
