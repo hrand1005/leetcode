4a95b1c9e3d6728e8faf395b2c979a044cc3835b
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-10 18:07:03 -0500

    Time: 46 ms (79.51%), Space: 14.3 MB (12.70%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         return self.generate(n, n, '', [])
@@ -17,3 +18,28 @@ class Solution:
             self.generate(lefts, rights-1, current + ')', answers)
         
         return answers
+"""
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        left, right = n, n
+        stack = [('(', left-1, right)]
+        combos = []
+        
+        while stack:
+            p = stack.pop()
+            if p[1] > p[2]:
+                continue
+            
+            if p[1] == 0 and p[2] == 0:
+                combos.append(p[0])
+                
+            if p[1] > 0:
+                new_left = (p[0] + '(', p[1] - 1, p[2])
+                stack.append(new_left)
+                
+            if p[2] > 0:
+                new_right = (p[0] + ')', p[1], p[2]-1)
+                stack.append(new_right)
+            
+        return combos
