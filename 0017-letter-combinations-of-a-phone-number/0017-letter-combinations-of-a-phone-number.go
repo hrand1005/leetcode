58df66b1a757fe6be9ac49e3c8f74c8811744ec5
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-09 20:20:35 -0500

    Time: 0 ms (100.00%), Space: 2.2 MB (16.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
@@ -2,20 +2,20 @@ import (
     "strings"
 )
 
+var numToLetters = map[string]string{
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
+
 func letterCombinations(digits string) []string {
     if len(digits) == 0 {
         return nil
-    }
-    
-    numToLetters := map[string]string{
-        "2": "abc",
-        "3": "def",
-        "4": "ghi",
-        "5": "jkl",
-        "6": "mno",
-        "7": "pqrs",
-        "8": "tuv",
-        "9": "wxyz",
     }
     
     combos := map[int][]string{}
