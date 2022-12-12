3e6282ee01017c9b815357ba8e9c44a286c41613
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-12 01:15:57 -0500

    Time: 0 ms (100.00%), Space: 2.2 MB (91.35%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.go
@@ -0,0 +1,53 @@
+import (
+    "math"
+)
+
+func checkOverflow(sign, quotient, toAdd int) (bool, int) {
+    if sign > 0 && quotient > math.MaxInt32 - toAdd  {
+        return true, math.MaxInt32
+    }
+    if sign < 0 && 0 - quotient < math.MinInt32 + toAdd {
+        return true, math.MinInt32
+    }
+    
+    return false, 0
+}
+
+func divide(dividend int, divisor int) int {
+    sign := 1
+    if divisor < 0 {
+        divisor = -divisor
+        sign = -sign
+    }
+    
+    if dividend < 0 {
+        dividend = -dividend
+        sign = -sign
+    }
+    
+    quotient := 0
+    counter := divisor
+    
+    for counter <= dividend {
+        multiplied := 1
+        for (counter << 1) <= dividend {
+            multiplied <<= 1
+            counter <<= 1
+        }
+        
+        overflowed, result := checkOverflow(sign, quotient, multiplied)
+        if overflowed {
+            return result
+        }
+        
+        dividend -= counter
+        quotient += multiplied
+        counter = divisor
+    }
+    
+    if sign < 0 {
+        return 0 - quotient
+    }
+    
+    return quotient
+}
