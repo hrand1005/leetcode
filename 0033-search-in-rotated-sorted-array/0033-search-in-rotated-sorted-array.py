06c1057c8ddcb0590717de8475ecd9dc6218335a
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-12 22:25:11 -0500

    Time: 91 ms (28.49%), Space: 14.4 MB (18.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         return self.recursive_search(nums, target, 0, len(nums)-1)
@@ -22,5 +23,26 @@ class Solution:
                 return self.recursive_search(nums, target, midpoint + 1, right)
             else:
                 return self.recursive_search(nums, target, left, midpoint - 1)
+ """           
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
         
+        while left <= right:
+            midpoint = (left + right) // 2
+            if nums[midpoint] == target:
+                return midpoint
             
+            if nums[left] <= nums[midpoint]:
+                if nums[left] <= target <= nums[midpoint]:
+                    right = midpoint - 1
+                else:
+                    left = midpoint + 1
+            else:
+                if nums[midpoint] <= target <= nums[right]:
+                    left = midpoint + 1
+                else:
+                    right = midpoint - 1
+            
+        return -1    
