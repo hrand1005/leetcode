ddf729c40e5e1fcd52e4c492abae00e3dc009708
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-12 22:34:25 -0500

    Time: 4 ms (70.92%), Space: 2.5 MB (100.00%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -0,0 +1,29 @@
+func recursiveSearch(nums []int, target, left, right int) int {
+    if left >= right {
+        if nums[left] == target {
+            return left
+        }
+        return -1
+    }
+    
+    midpoint := (left + right) / 2
+    if nums[midpoint] == target {
+        return midpoint
+    }
+    
+    if nums[left] <= nums[midpoint] {
+        if nums[left] <= target && target < nums[midpoint] {
+            return recursiveSearch(nums, target, left, midpoint - 1)
+        } 
+        return recursiveSearch(nums, target, midpoint + 1, right)
+    } else {
+        if nums[midpoint] < target && target <= nums[right] {
+            return recursiveSearch(nums, target, midpoint + 1, right)
+        }
+        return recursiveSearch(nums, target, left, midpoint - 1)
+    }
+}
+
+func search(nums []int, target int) int {
+    return recursiveSearch(nums, target, 0, len(nums) - 1)
+}
