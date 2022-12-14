5a81879a404575bca702b97c109049921af86da3
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-13 23:22:00 -0500

    Time: 8 ms (30.58%), Space: 2.9 MB (49.60%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.go
@@ -0,0 +1,68 @@
+const blank = byte(46)
+
+func isValidSudoku(board [][]byte) bool {
+    fmt.Println(board)
+    for i := 0; i < 9; i++ {
+        if !validRow(board, i) {
+            return false
+        }
+        
+        if !validColumn(board, i) {
+            return false
+        }
+        
+        if !validSquare(board, i) {
+            return false
+        }
+    }
+    
+    return true
+}
+
+func validRow(board [][]byte, index int) bool {
+    seen := map[byte]bool{}
+    row := board[index]
+    
+    for _, v := range row {
+        n := v
+        if _, ok := seen[n]; ok && n != blank {
+            return false
+        }
+        seen[n] = true
+    }
+    
+    return true
+}
+
+func validColumn(board [][]byte, index int) bool {
+    seen := map[byte]bool{}
+    
+    for i := 0; i < 9; i++ {
+        n := board[i][index]
+        if _, ok := seen[n]; ok && n != blank {
+            return false
+        }
+        seen[n] = true
+    }
+    
+    return true
+}
+
+func validSquare(board [][]byte, index int) bool {
+    seen := map[byte]bool{}
+    
+    iOffset := (index % 3) * 3
+    jOffset := (index / 3) * 3
+    
+    for i := 0; i < 3; i++ {
+        for j := 0; j < 3; j++ {
+            n := board[iOffset + i][jOffset + j]
+            if _, ok := seen[n]; ok && n != blank {
+                return false
+            }
+            seen[n] = true
+        }
+    }
+    
+    return true
+}
