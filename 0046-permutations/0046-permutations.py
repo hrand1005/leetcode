745910e154bf8d53eb5cb31a7428e2a507f1aa56
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-14 21:25:31 -0500

    Time: 43 ms (90.54%), Space: 14 MB (84.54%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         perms = {
@@ -15,5 +16,20 @@ class Solution:
                             perms[j].append(p + [n])
                     
         return perms[len(nums)]            
-            
-            
+"""            
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        stack = [([], nums)]
+        result = []
+        
+        while stack:
+            perms, remaining = stack.pop()
+            if not remaining:
+                result.append(perms)
+            for i in range(len(remaining)):
+                new_perms = perms + [remaining[i]]
+                new_remaining = remaining[:i] + remaining[i+1:]
+                stack.append((new_perms, new_remaining))
+                
+        return result
