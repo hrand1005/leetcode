d0b4e4068d7885143a32926ac26a28f1eb1dc770
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-14 19:11:47 -0500

    Time: 42 ms (96.75%), Space: 13.9 MB (57.20%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def countAndSay(self, n: int) -> str:
         return self.countAndSayRecursive(n, 1, "1")
@@ -19,5 +20,25 @@ class Solution:
         
         return self.countAndSayRecursive(n, i+1, new_say + str(count) + cur)
                 
-            
+"""
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        say = {
+            1: "1",
+        }
+        for i in range(2, n+1):
+            this = say[i-1]
+            cur = this[0]
+            count = 1
+            new = ""
+            for j in range(1, len(this)):
+                if this[j] == this[j-1]:
+                    count += 1
+                else:
+                    new += f'{count}{cur}'
+                    count = 1
+                    cur = this[j]
+            say[i] = new + f'{count}{cur}'
         
+        return say[n]
+            
