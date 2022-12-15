fbea5d380b6f4723865f2615c9a86894becefa9d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-14 21:57:09 -0500

    Time: 4 ms (44.38%), Space: 3.6 MB (9.47%) - LeetHub

---
+++ 0046-permutations/0046-permutations.go
@@ -0,0 +1,43 @@
+type permTuple struct {
+    perm []int
+    remaining []int
+}
+
+func permute(nums []int) [][]int {
+    stack := []permTuple{
+        {
+            perm: []int{},
+            remaining: nums,
+        },
+    }
+    
+    allPerms := [][]int{}
+    
+    for len(stack) > 0 {
+        popped := stack[0]
+        stack = stack[1:]
+        
+        if len(popped.remaining) == 0 {
+            allPerms = append(allPerms, popped.perm)
+        }
+        
+        for i := 0; i < len(popped.remaining); i++ {
+            newPerm := []int{}
+            newPerm = append(newPerm, popped.perm...)
+            newPerm = append(newPerm, popped.remaining[i])
+            
+            newRemaining := []int{}
+            newRemaining = append(newRemaining, popped.remaining[:i]...)
+            newRemaining = append(newRemaining, popped.remaining[i+1:]...)
+            
+            newPermTuple := permTuple{
+                perm: newPerm,
+                remaining: newRemaining,
+            }
+            
+            stack = append(stack, newPermTuple)
+        }
+    }
+    
+    return allPerms
+}
