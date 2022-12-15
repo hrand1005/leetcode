6c8e8a6a265edbe3d8106af02cb4345ded2943a1
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-14 19:27:50 -0500

    Time: 13 ms (40.29%), Space: 7.3 MB (45.50%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.go
+++ 0038-count-and-say/0038-count-and-say.go
@@ -1,3 +1,4 @@
+/*
 import (
     "strings"
 )
@@ -24,6 +25,34 @@ func countAndSay(n int) string {
         }
         newSay.WriteString(fmt.Sprintf("%v%v", count, current))
         say[i] = newSay.String()
+    }
+    
+    return say[n]
+}
+*/
+
+func countAndSay(n int) string {
+    say := map[int]string{
+        1: "1",
+    }
+    
+    for i := 2; i < n+1; i++ {
+        sayThis := say[i-1]
+        newSay := ""
+        count := 1
+        current := string(sayThis[0])
+        
+        for j := 1; j < len(sayThis); j++ {
+            if sayThis[j] == sayThis[j-1] {
+                count++
+            } else {
+                newSay += fmt.Sprintf("%v%v", count, current)
+                current = string(sayThis[j])
+                count = 1
+            }
+        }
+        newSay += fmt.Sprintf("%v%v", count, current)
+        say[i] = newSay
     }
     
     return say[n]
