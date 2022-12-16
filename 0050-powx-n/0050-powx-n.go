edc57209a5ddbb36a35a66b3f0f9b4aa854a68ba
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-16 18:03:27 -0500

    Time: 0 ms (100.00%), Space: 1.9 MB (88.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.go
+++ 0050-powx-n/0050-powx-n.go
@@ -1,3 +1,4 @@
+/*
 func myPow(x float64, n int) float64 {
     if n == 0 {
         return 1
@@ -26,4 +27,21 @@ func myPow(x float64, n int) float64 {
         return 1 / result
     }
     return result
+}
+*/
+
+func myPow(x float64, n int) float64 {
+    if n == 0 {
+        return 1
+    }
+    
+    if n < 0 {
+        return 1 / myPow(x, -n)
+    }
+    
+    if n % 2 != 0 {
+        return x * myPow(x, n-1)
+    }
+    
+    return myPow(x*x, n/2)
 }
