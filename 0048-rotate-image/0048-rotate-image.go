c05f3ffafcee3105cc0bf7e235b8618d1d895405
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-15 19:18:17 -0500

    Time: 2 ms (63.60%), Space: 2.1 MB (80.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,3 +1,4 @@
+/*
 func rotate(matrix [][]int)  {
     end := len(matrix) - 1
     
@@ -8,6 +9,20 @@ func rotate(matrix [][]int)  {
     for i := 0; i < len(matrix); i++ {
         for j := i; j < len(matrix); j++ {
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+    }
+}
+*/
+
+func rotate(matrix [][]int)  {
+    quartileHeight := len(matrix) / 2
+    quartileWidth := len(matrix) - (len(matrix) / 2)
+    end := len(matrix) - 1
+    
+    for i := 0; i < quartileHeight; i++ {
+        for j := 0; j < quartileWidth; j++ {
+            matrix[i][j], matrix[j][end-i], matrix[end-i][end-j], matrix[end-j][i] = 
+            matrix[end-j][i], matrix[i][j], matrix[j][end-i], matrix[end-i][end-j]
         }
     }
 }
