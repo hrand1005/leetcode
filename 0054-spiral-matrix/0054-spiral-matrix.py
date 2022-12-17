1b08b9d5e60da1c9fa3cf3cf5e19d15dbf03a2e4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-17 10:45:14 -0500

    Time: 23 ms (99.33%), Space: 13.9 MB (82.90%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,39 +1,3 @@
-"""
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if len(matrix) == 0:
-            return []
-        if len(matrix) == 1:
-            return matrix[0]
-        
-        result, inner_matrix = self.pop_border(matrix) 
-        return result + self.spiralOrder(inner_matrix)
-        
-    def pop_border(self, matrix: List[List[int]]) -> (List, List[List[int]]):
-        # shave top
-        border = matrix.pop(0)
-        if len(matrix[0]) == 0:
-            return border, [[]]
-        
-        # shave right
-        for i in range(0, len(matrix)-1):
-            right_elem = matrix[i].pop(len(matrix[i])-1)
-            border.append(right_elem)
-            
-        # shave bottom    
-        reverse_bottom = matrix.pop(len(matrix)-1)[::-1]
-        border.extend(reverse_bottom)
-        if len(matrix) == 0 or len(matrix[0]) == 0:
-            return border, matrix
-        
-        # shave left
-        for j in range(len(matrix)-1, 0, -1):
-            left_elem = matrix[j].pop(0)
-            border.append(left_elem)
-        
-        return border, matrix  
-"""
-
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         if len(matrix) == 0:
