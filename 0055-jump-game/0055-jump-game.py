b38b9abdef99255a8846702e8d871d211da310b4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-17 12:04:20 -0500

    Time: 489 ms (92.92%), Space: 15.3 MB (49.53%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,45 @@
+"""
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        stack = [(0, nums[0])]
+        target = len(nums) - 1
+        visited = {}
+        
+        while stack:
+            idx, val = stack.pop()
+            if idx == target:
+                return True
+            
+            if visited.get(idx) != None:
+                continue
+            
+            visited[idx] = True
+            for i in range(val, 0, -1):
+                if idx + i > len(nums) - 1:
+                    return True
+                if visited.get(idx+i) == None:
+                    stack.append((idx+i, nums[idx+i]))
+            
+        return False
+        
+"""
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        target = len(nums) - 1
+        max_reach = nums[0]
+        
+        for i in range(1, len(nums)):
+            if max_reach < i:
+                return False
+            
+            if nums[i] == 0:
+                continue
+            
+            max_reach = max(max_reach, i + nums[i])
+            if max_reach >= target:
+                return True
+        
+        return max_reach >= target
+             
+            
