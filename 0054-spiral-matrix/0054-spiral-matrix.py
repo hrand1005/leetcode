c0d0bd700659e1043d9ad994d32eada778a0eba0
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-17 10:49:33 -0500

    Time: 40 ms (77.25%), Space: 14 MB (34.50%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -9,23 +9,23 @@ class Solution:
         return result + self.spiralOrder(matrix)
         
     def pop_border(self, matrix: List[List[int]]) -> List[int]:
-        # shave top
+        # pop top
         border = matrix.pop(0)
         if len(matrix[0]) == 0:
             return border
         
-        # shave right
+        # pop right
         for i in range(0, len(matrix)-1):
             right_elem = matrix[i].pop(len(matrix[i])-1)
             border.append(right_elem)
             
-        # shave bottom    
+        # pop bottom    
         reverse_bottom = matrix.pop(len(matrix)-1)[::-1]
         border.extend(reverse_bottom)
         if len(matrix) == 0 or len(matrix[0]) == 0:
             return border
         
-        # shave left
+        # pop left
         for j in range(len(matrix)-1, 0, -1):
             left_elem = matrix[j].pop(0)
             border.append(left_elem)
