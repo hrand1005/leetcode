cebd739a8ba24c7bb3ee7fc039827ba455109dcf
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-16 21:21:07 -0500

    Time: 816 ms (83.30%), Space: 27.9 MB (79.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -24,7 +24,7 @@ class Solution:
             sums[i] = max(sums[i-1] + nums[i], nums[i])
         
         return max(sums)
-"""
+"""        
     
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
