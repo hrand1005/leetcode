a6eb61c4c9f537432976995eb758634df8b7ae2d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-17 10:42:22 -0500

    Time: 37 ms (83.26%), Space: 13.9 MB (82.90%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -9,25 +9,23 @@ class Solution:
         return result + self.spiralOrder(inner_matrix)
         
     def pop_border(self, matrix: List[List[int]]) -> (List, List[List[int]]):
-        outer = matrix.pop(0)
+        border = matrix.pop(0)
         
         if len(matrix[0]) == 0:
-            return outer, [[]]
+            return border, [[]]
         
         for i in range(0, len(matrix)-1):
             right_elem = matrix[i].pop(len(matrix[i])-1)
-            outer.append(right_elem)
+            border.append(right_elem)
         
         reverse_bottom = matrix.pop(len(matrix)-1)[::-1]
-        outer.extend(reverse_bottom)
+        border.extend(reverse_bottom)
         
         if len(matrix) == 0 or len(matrix[0]) == 0:
-            return outer, matrix
+            return border, matrix
         
         for j in range(len(matrix)-1, 0, -1):
             left_elem = matrix[j].pop(0)
-            outer.append(left_elem)
+            border.append(left_elem)
         
-        return outer, matrix  
-    
-    
+        return border, matrix  
