dc52ae6a59b20690c0defdbe22d450b90612464f
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-19 10:01:47 -0500

    Time: 45 ms (70.06%), Space: 14 MB (13.96%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,37 @@
+"""
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        stack = [ (0, 0) ]
+        finish = (m-1, n-1)
+        paths = 0
+        while stack:
+            last = stack.pop()
+            if last == finish:
+                paths += 1
+                continue
+            
+            neighbors = self.get_neighbors(last, m, n)
+            stack.extend(neighbors)
+            
+        return paths    
+    
+    def get_neighbors(self, node: tuple[int, int], m: int, n: int) -> List[tuple[int, int]]:
+        neighbors = []
+        if node[0] < m - 1:
+            neighbors.append((node[0]+1, node[1]))
+            
+        if node[1] < n - 1:
+            neighbors.append((node[0], node[1]+1))
+        
+        return neighbors
+"""
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        table = [ [1] * n for i in range(m) ]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                table[i][j] = table[i-1][j] + table[i][j-1]
+        
+        return table[-1][-1]
