071fe1c27e348ec62dbd35cf3cc355955aa82b19
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-19 10:05:16 -0500

    Time: 57 ms (50.61%), Space: 13.9 MB (74.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -24,14 +24,23 @@ class Solution:
             neighbors.append((node[0], node[1]+1))
         
         return neighbors
-"""
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         table = [ [1] * n for i in range(m) ]
-        
         for i in range(1, m):
             for j in range(1, n):
                 table[i][j] = table[i-1][j] + table[i][j-1]
         
         return table[-1][-1]
+"""
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        current_line = [1] * n
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                current_line[j] += current_line[j-1]
+        
+        return current_line[-1]
