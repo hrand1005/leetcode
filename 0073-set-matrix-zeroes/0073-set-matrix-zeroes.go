71b2e86ed052b60689c8172a7451d4d6355efd1b
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-19 16:01:39 -0500

    Time: 26 ms (45.74%), Space: 6.2 MB (96.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
@@ -1,3 +1,4 @@
+/*
 func setZeroes(matrix [][]int)  {
     zeroRows := map[int]bool{}
     zeroCols := map[int]bool{}
@@ -16,6 +17,47 @@ func setZeroes(matrix [][]int)  {
             if zeroRows[i] || zeroCols[j] {
                 matrix[i][j] = 0
             }
+        }
+    }
+    
+    return
+}
+*/
+
+func setZeroes(matrix [][]int)  {
+    firstRowZero := false
+    for _, v := range matrix[0] {
+        if v == 0 {
+            firstRowZero = true
+            break
+        }
+    }
+    
+    for i := 1; i < len(matrix); i++ {
+        for j := 0; j < len(matrix[i]); j++ {
+            if matrix[i][j] == 0 {
+                matrix[0][j] = 0
+                matrix[i][0] = 0
+            }
+        }
+    }
+    
+    for i := 1; i < len(matrix); i++ {
+        if matrix[i][0] == 0 {
+            for j := range matrix[i] {
+                matrix[i][j] = 0
+            }
+        }
+        for j := 0; j < len(matrix[i]); j++ {
+            if matrix[0][j] == 0 {
+                matrix[i][j] = 0
+            }
+        }
+    }
+    
+    if firstRowZero {
+        for i := 0; i < len(matrix[0]); i++ {
+            matrix[0][i] = 0
         }
     }
     
