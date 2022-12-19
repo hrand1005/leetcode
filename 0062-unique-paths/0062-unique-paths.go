6fc024b24fe5d0011af031a3fd5afca3870a09ad
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-19 10:24:15 -0500

    Time: 1 ms (52.82%), Space: 1.9 MB (74.25%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.go
@@ -0,0 +1,24 @@
+func uniquePaths(m int, n int) int {
+    table := createTable(m, n)
+    for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            table[i][j] = table[i-1][j] + table[i][j-1]
+        }
+    }
+    
+    return table[m-1][n-1]
+}
+
+func createTable(m, n int) [][]int {
+    row := make([]int, n)
+    for i := 0; i < n; i++ {
+        row[i] = 1
+    }
+    
+    table := make([][]int, m)
+    for i := 0; i < m; i++ {
+        table[i] = row
+    }
+    
+    return table
+}
