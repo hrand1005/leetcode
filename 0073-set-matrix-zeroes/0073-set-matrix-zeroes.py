471ade1c1f40e9eea66505cca98cfadee09499a3
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-19 15:40:52 -0500

    Time: 165 ms (73.92%), Space: 14.8 MB (55.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,8 +1,7 @@
+"""
+# O(m + n)
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         zero_rows = set()
         zero_cols = set()
         for i in range(len(matrix)):
@@ -15,5 +14,31 @@ class Solution:
             for j in range(len(matrix[i])):
                 if i in zero_rows or j in zero_cols:
                     matrix[i][j] = 0
+        
+        return
+"""
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        first_row_zero = 0 in matrix[0]
+        
+        for i in range(1, len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+        
+        for i in range(1, len(matrix)):
+            if 0 in matrix[i]:
+                matrix[i] = [0] * len(matrix[i])
+            else:    
+                for j in range(0, len(matrix[i])):
+                    print(f'(0, j): (0, {j})')
+                    if matrix[0][j] == 0:
+                        print(f'setting ({i}, {j}) to zero')
+                        matrix[i][j] = 0
+                        
+        if first_row_zero:
+            matrix[0] = [0] * len(matrix[0])
         
         return
