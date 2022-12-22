a878fdd2a966788ddc39a02946d88a113132d26b
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-22 10:50:15 -0500

    Time: 41 ms (81.48%), Space: 14.1 MB (84.41%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -9,7 +9,7 @@ class Solution:
         if root == None:
             return []
         
-        values = [[root.val]]
+        values = []
         queue = [(root, 0)]
         
         while queue:
@@ -17,20 +17,12 @@ class Solution:
             if node == None:
                 continue
                 
-            child_vals = []
-            if node.left != None:
-                child_vals.append(node.left.val)
-            if node.right != None:
-                child_vals.append(node.right.val)
+            if len(values) < level + 1:
+                values.append([node.val])
+            else:
+                values[level].append(node.val)
                 
-            new_level = level+1
-            if len(child_vals) != 0:
-                if len(values) < new_level + 1:
-                    values.append(child_vals)
-                else:    
-                    values[new_level].extend(child_vals)    
-                
-            queue.append((node.left, new_level))
-            queue.append((node.right, new_level))
+            queue.append((node.left, level+1))
+            queue.append((node.right, level+1))
         
         return values
