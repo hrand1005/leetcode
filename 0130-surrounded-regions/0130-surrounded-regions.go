5bba0380ea7103b3f4c6f4f6a5c7b74d627117ba
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-29 18:39:24 -0500

    Time: 36 ms (50.51%), Space: 6.5 MB (76.02%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.go
+++ 0130-surrounded-regions/0130-surrounded-regions.go
@@ -6,6 +6,36 @@ func (p point) String() string {
     return fmt.Sprintf("%v,%v", p.i, p.j)
 }
 
+func getBorderPoints(board [][]byte) []point {
+    entryPoints := make([]point, 0, 4*len(board))
+    // scan top
+    for i, v := range board[0] {
+        if v == 'O' {
+            entryPoints = append(entryPoints, point{ 0, i })
+        }
+    }
+    // scan left
+    for i, v := range board {
+        if v[0] == 'O' {
+            entryPoints = append(entryPoints, point{ i, 0 })
+        }
+    }
+    // scan right
+    for i, v := range board {
+        if v[len(v)-1] == 'O' {
+            entryPoints = append(entryPoints, point{ i, len(v)-1 })
+        }
+    }
+    // scan bottom
+    for i, v := range board[len(board)-1] {
+        if v == 'O' {
+            entryPoints = append(entryPoints, point{ len(board)-1, i })
+        }
+    }
+    
+    return entryPoints
+}
+
 func solve(board [][]byte)  {
     queue := getBorderPoints(board)
     visited := make(map[string]bool)
@@ -39,37 +69,4 @@ func solve(board [][]byte)  {
             }
         }
     }
-}
-
-func getBorderPoints(board [][]byte) []point {
-    entryPoints := make([]point, 0, 2*len(board) + 2*len(board[0]))
-    // scan top
-    for i, v := range board[0] {
-        if v == 'O' {
-            entryPoints = append(entryPoints, point{ 0, i })
-        }
-    }
-    
-    // scan left
-    for i, v := range board {
-        if v[0] == 'O' {
-            entryPoints = append(entryPoints, point{ i, 0 })
-        }
-    }
-    
-    // scan right
-    for i, v := range board {
-        if v[len(v)-1] == 'O' {
-            entryPoints = append(entryPoints, point{ i, len(v)-1 })
-        }
-    }
-    
-    // scan bottom
-    for i, v := range board[len(board)-1] {
-        if v == 'O' {
-            entryPoints = append(entryPoints, point{ len(board)-1, i })
-        }
-    }
-    
-    return entryPoints
 }
