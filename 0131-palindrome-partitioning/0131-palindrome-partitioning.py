5bacbfa5d1fca3940447640c658723deda4ab8b1
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-29 21:48:10 -0500

    Time: 805 ms (67.76%), Space: 31.4 MB (14.66%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        if len(s) == 0:
+            return [[]]
+        
+        if len(s) == 1:
+            return [[s]]
+        
+        all_pals = []
+        for i in range(len(s), 0, -1):
+            candidate = s[:i]
+            print(candidate)
+            if self.is_palindrome(candidate):
+                print("IS PALINDROME")
+                remainders = self.partition(s[i:])
+                for r in remainders:
+                    all_pals.append([candidate]+r)
+        
+        return all_pals
+                
+    def is_palindrome(self, s: str) -> bool:
+        if len(s) <= 1:
+            return True
+        if s[0] == s[-1]:
+            return self.is_palindrome(s[1:-1])
+        return False
