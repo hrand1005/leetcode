2e25f02d715aeae9f3a3c20f7f45e37f80651a3b
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-30 17:23:21 -0500

    Time: 82 ms (90.78%), Space: 9.1 MB (99.29%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.go
@@ -0,0 +1,53 @@
+/*
+func canCompleteCircuit(gas []int, cost []int) int {
+    if sum(gas) < sum(cost) {
+        return -1
+    }
+    
+    lastDiff := -1
+    for i := 0; i < len(gas); i++ {
+        if cost[i] <= gas[i] && lastDiff < 0 {
+            if canComplete(i, gas, cost) {
+                return i
+            }
+        }
+    }
+    return -1
+}
+
+func canComplete(start int, gas, cost []int) bool {
+    tank := 0
+    for i := 0; i < len(gas); i++ {
+        index := (start + i) % len(gas)
+        tank += gas[index] - cost[index]
+        if tank < 0 {
+            return false
+        }
+    }
+    return true
+}
+
+func sum(s []int) int {
+    sum := 0
+    for i := 0; i < len(s); i++ {
+        sum += s[i]
+    }
+    return sum
+}
+*/
+
+func canCompleteCircuit(gas []int, cost []int) int {
+    start, tank, total := 0, 0, 0
+    for i := 0; i < len(gas); i++ {
+        total += gas[i] - cost[i]
+        tank += gas[i] - cost[i]
+        if tank < 0 {
+            tank = 0
+            start = i + 1
+        }
+    }
+    if total < 0 {
+        return -1
+    }
+    return start
+}
