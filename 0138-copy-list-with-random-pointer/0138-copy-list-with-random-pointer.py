88f568b3cc71ca87dac0ca4fb6c0b7cf5a9c5204
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-30 20:46:04 -0500

    Time: 47 ms (67.44%), Space: 15.1 MB (16.52%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -5,7 +5,6 @@ class Node:
         self.val = int(x)
         self.next = next
         self.random = random
-"""
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
@@ -32,5 +31,32 @@ class Solution:
                 for j in range(len(orig_list)):
                     if orig_list[j] == orig_rand:
                         copy_list[i].random = copy_list[j]
+        
+        return head_copy
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head == None:
+            return None
+        
+        head_copy = Node(head.val)
+        cur, cur_copy = head, head_copy
+        orig_to_copy = {}
+        while cur:
+            if cur.next != None:
+                cur_copy.next = Node(cur.next.val)
+            orig_to_copy[cur] = cur_copy
+            cur = cur.next    
+            cur_copy = cur_copy.next
+        
+        cur, cur_copy = head, head_copy
+        while cur:
+            if cur.random == None:
+                cur_copy.random = None
+            else:    
+                cur_copy.random = orig_to_copy[cur.random]
+            cur = cur.next
+            cur_copy = cur_copy.next
         
         return head_copy
