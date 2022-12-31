db6f94b9c5ed9e6962ab434602f5544df42de648
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2022-12-31 14:14:16 -0500

    Time: 859 ms (89.38%), Space: 76.3 MB (31.64%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,5 +1,5 @@
+"""
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
@@ -24,7 +24,54 @@ class LRUCache:
         self.queue.append(key)
         self.cache[key] = value
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+"""
+class Node:
+    def __init__(self, key=-1, val=-1, next=None, prev=None):
+        self.val = val
+        self.key = key
+        self.next = next
+        self.prev = prev
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.map = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        node = self.map.get(key)
+        if node != None:
+            self._remove_node(node)
+            self._add_to_tail(node)
+            return node.val
+        return -1
+        
+    def put(self, key: int, value: int) -> None:
+        node = self.map.get(key)
+        if node != None:
+            self._remove_node(node)
+            node.val = value
+        else:
+            if len(self.map) == self.capacity:
+                self.map.pop(self.head.next.key)
+                self._remove_node(self.head.next)
+            node = Node(key=key, val=value)
+        self._add_to_tail(node)
+        self.map[key] = node
+            
+    def _remove_node(self, node: Node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+   
+    def _add_to_tail(self, node: Node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+
