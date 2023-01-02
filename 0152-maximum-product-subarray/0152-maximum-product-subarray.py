ecf7e8478c0d8f15e298b19fb793290b0a0e6ae3
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-01 20:14:42 -0500

    Time: 77 ms (98.38%), Space: 14.3 MB (78.76%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -2,16 +2,9 @@ MIN_INT = -(1 << 31)
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        last_zero = -1
-        candidates = []
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                candidates.append(nums[last_zero+1:i])
-                last_zero = i
-        candidates.append(nums[last_zero+1:])        
-        
+        candidates = self.find_candidates(nums)
         max_product = MIN_INT
-        if last_zero != -1:
+        if len(candidates) > 1:
             max_product = 0
         
         while candidates:
@@ -22,15 +15,25 @@ class Solution:
                     if c[i] < 0:
                         candidates.append(c[:i])
                         candidates.append(c[i+1:])
-                        
             max_product = max(max_product, prod)            
             
         return max_product
     
+    # finds candidate subarrays for max product
+    def find_candidates(self, nums: List[int]) -> List[int]:
+        last_zero = -1
+        candidates = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                candidates.append(nums[last_zero+1:i])
+                last_zero = i
+        candidates.append(nums[last_zero+1:])        
+        return candidates
+    
+    # returns min int32 val if nums is empty
     def product(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return MIN_INT
-        
         product = 1
         for n in nums:
             product *= n
