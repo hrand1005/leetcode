0309c0822914732f99c493726bf0d97823adfabf
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-02 14:43:31 -0500

    Time: 91 ms (59.04%), Space: 18.4 MB (14.78%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,3 +1,4 @@
+"""
 class MinStack:
 
     def __init__(self):
@@ -25,10 +26,27 @@ class MinStack:
             self.min_val = self.stack[0]
             for n in self.stack:
                 self.min_val = min(self.min_val, n)
+"""
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+class MinStack:
+
+    def __init__(self):
+        # stack of (min, cur) tuples
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append((val, val))
+        else:
+            prev_min = self.stack[-1][0]
+            new_min = min(prev_min, val)
+            self.stack.append((new_min, val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][1]
+        
+    def getMin(self) -> int:
+        return self.stack[-1][0]
