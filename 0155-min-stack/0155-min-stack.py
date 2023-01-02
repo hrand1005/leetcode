ef569638aa13dbd874e6d2920c7810659429e0f7
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-02 14:51:23 -0500

    Time: 118 ms (51.64%), Space: 19.2 MB (5.80%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -26,7 +26,6 @@ class MinStack:
             self.min_val = self.stack[0]
             for n in self.stack:
                 self.min_val = min(self.min_val, n)
-"""
 
 class MinStack:
 
@@ -50,3 +49,31 @@ class MinStack:
         
     def getMin(self) -> int:
         return self.stack[-1][0]
+"""
+class Node:
+    def __init__(self, val=0, min_val=0, prev=None):
+        self.val = val
+        self.min_val = min_val
+        self.prev = prev
+
+class MinStack:
+
+    def __init__(self):
+        self.cur = None
+
+    def push(self, val: int) -> None:
+        if self.cur == None:
+            self.cur = Node(val=val, min_val=val)
+        else:
+            prev_min = self.cur.min_val
+            new_min = min(prev_min, val)
+            self.cur = Node(val=val, min_val=new_min, prev=self.cur)
+
+    def pop(self) -> None:
+        self.cur = self.cur.prev
+
+    def top(self) -> int:
+        return self.cur.val
+        
+    def getMin(self) -> int:
+        return self.cur.min_val
