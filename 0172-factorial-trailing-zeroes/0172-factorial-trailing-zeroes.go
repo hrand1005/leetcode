5adaafa83209df370330f4ae4eb2e8d6410cee13
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-04 23:34:36 -0500

    Time: 0 ms (100.00%), Space: 2 MB (58.49%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.go
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.go
@@ -1,8 +1,20 @@
+/*
 func trailingZeroes(n int) int {
     zeroes := 0
     for 0 < n {
         n /= 5
         zeroes += n
+    }
+    return zeroes
+}
+*/
+
+func trailingZeroes(n int) int {
+    divisor := 5
+    zeroes := 0
+    for divisor <= n {
+        zeroes += n / divisor
+        divisor *= 5
     }
     return zeroes
 }
