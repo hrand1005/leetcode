b59bf2e8b38ab7f9a601dd37ed3b782d835b7eab
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-05 21:35:33 -0500

    Time: 219 ms (92.12%), Space: 25.4 MB (78.80%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -10,7 +10,6 @@ class Solution:
             nums[i] = tmp[i]
         
         return
-"""
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
@@ -23,3 +22,17 @@ class Solution:
             nums[i] = new_loc[i]
         
         return
+"""
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k %= len(nums)
+        self.reverse(nums, 0, len(nums)-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, len(nums)-1)
+    
+    def reverse(self, nums: List[int], left: int, right: int) -> None:
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
