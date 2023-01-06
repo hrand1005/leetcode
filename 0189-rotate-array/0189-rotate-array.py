9ed52d282235404e58d2437b48aa9db5b8d92bf9
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-05 21:02:31 -0500

    Time: 246 ms (70.83%), Space: 29.3 MB (7.78%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,6 @@
+"""
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         n = len(nums)
         tmp = [0] * len(nums)
         for i in range(n):
@@ -10,5 +8,18 @@ class Solution:
         
         for i in range(n):
             nums[i] = tmp[i]
+        
+        return
+"""
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        new_loc = {}
+        for i in range(n):
+            new_loc[(i+k)%n] = nums[i]
+        
+        for i in range(n):
+            nums[i] = new_loc[i]
         
         return
