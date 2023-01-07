b7a56444495f3231d4d9413cfcf3993c99b89ae9
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-07 15:57:30 -0500

    Time: 16 ms (66.57%), Space: 6.5 MB (36.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.go
+++ 0207-course-schedule/0207-course-schedule.go
@@ -1,3 +1,4 @@
+/*
 func canFinish(numCourses int, prerequisites [][]int) bool {
     if len(prerequisites) == 0 {
         return true
@@ -34,5 +35,54 @@ func canFinishRecursive(course int, visited, finishable map[int]bool, prerequisi
     }
     
     finishable[course] = true
+    return true
+}
+*/
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+    // prereqFor maps each course to a slice of
+    // courses that require it as a prerequisite
+    prereqFor := make(map[int][]int, numCourses)
+    
+    // prereqCount counts maps a course to the 
+    // number of prerequisites it requires
+    prereqCount := make(map[int]int, numCourses)
+    
+    // initialize the declared graph and map
+    for _, prereq := range prerequisites {
+        course, req := prereq[0], prereq[1]
+        prereqFor[req] = append(prereqFor[req], course)
+        prereqCount[course]++
+    }
+    
+    // initialize a queue with valid starting courses
+    // (courses with no prerequisites)
+    queue := make([]int, 0, numCourses)
+    for course, _ := range prereqFor {
+        if prereqCount[course] == 0 {
+            queue = append(queue, course)
+        }
+    }
+    
+    // bfs through courses, appending courses only 
+    // after their prerequisites have been visited
+    for len(queue) != 0 {
+        req := queue[0]
+        queue = queue[1:]
+        for _, course := range prereqFor[req] {
+            prereqCount[course]--
+            if prereqCount[course] == 0 {
+                queue = append(queue, course)
+            }
+        }
+    }
+    
+    // check that each course could have their 
+    // prerequisites met during bfs traversal
+    for _, count := range prereqCount {
+        if count != 0 {
+            return false
+        }
+    }
     return true
 }
