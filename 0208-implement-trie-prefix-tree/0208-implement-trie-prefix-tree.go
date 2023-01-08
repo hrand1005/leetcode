34fbce0688e34bcc4ffbebcc982042d9987b28fc
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-07 19:49:20 -0500

    Time: 61 ms (79.88%), Space: 15.6 MB (69.54%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.go
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.go
@@ -1,3 +1,4 @@
+/*
 type Trie struct {
     words map[string]bool
     prefixes map[string]bool
@@ -28,12 +29,62 @@ func (this *Trie) Search(word string) bool {
 func (this *Trie) StartsWith(prefix string) bool {
     return this.prefixes[prefix] || this.words[prefix]
 }
+*/
+
+type Node struct {
+    m map[rune]Node
+}
+
+type Trie struct {
+    trie Node
+}
 
 
-/**
- * Your Trie object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Insert(word);
- * param_2 := obj.Search(word);
- * param_3 := obj.StartsWith(prefix);
- */
+func Constructor() Trie {
+    return Trie{
+        trie: Node{
+            m: map[rune]Node{},
+        },
+    }
+}
+
+
+func (this *Trie) Insert(word string)  {
+    trie := this.trie
+    for _, l := range word {
+        if _, ok := trie.m[l]; !ok {
+            trie.m[l] = Node{
+                m: map[rune]Node{},
+            }
+        }
+        trie = trie.m[l]
+    }
+    trie.m['0'] = Node{
+        m: map[rune]Node{},
+    }
+}
+
+
+func (this *Trie) Search(word string) bool {
+    trie := this.trie
+    for _, l := range word {
+        if _, ok := trie.m[l]; !ok {
+            return false
+        } 
+        trie = trie.m[l]
+    }
+    _, ok := trie.m['0']
+    return ok
+}
+
+
+func (this *Trie) StartsWith(prefix string) bool {
+    trie := this.trie
+    for _, l := range prefix {
+        if _, ok := trie.m[l]; !ok {
+            return false
+        } 
+        trie = trie.m[l]
+    }
+    return true
+}
