32cf0e9ceffc08d6895789d3b287b711c97c4f1e
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-07 22:44:30 -0500

    Time: 20 ms (83.56%), Space: 4.5 MB (85.59%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.go
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -1,3 +1,4 @@
+/*
 func isPalindrome(x int) bool {
     xStr := strconv.Itoa(x)
     return palindrome(xStr)
@@ -11,4 +12,21 @@ func palindrome(s string) bool {
         return false
     }
     return palindrome(s[1:len(s)-1])
+}
+*/
+
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+    
+    before := x
+    after := 0
+    for 0 < x {
+        after *= 10
+        after += x % 10
+        x /= 10
+    }
+    
+    return before == after
 }
