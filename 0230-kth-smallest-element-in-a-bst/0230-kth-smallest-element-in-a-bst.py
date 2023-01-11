9cd65a8e1c8ba2f858c5c28461cb99f5e2e3ab12
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-10 20:22:44 -0500

    Time: 51 ms (90.25%), Space: 18 MB (88.14%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        smallest = []
+        self.kth_smallest_recursive(root, k, smallest)
+        return smallest[-1]
+    
+    def kth_smallest_recursive(self, root: Optional[TreeNode], k: int, smallest: List[int]):
+        if root == None:
+            return 
+        
+        if len(smallest) < k:
+            self.kth_smallest_recursive(root.left, k, smallest)
+            
+        if len(smallest) < k:
+            smallest.append(root.val)
+            
+        if len(smallest) < k:
+            self.kth_smallest_recursive(root.right, k, smallest)
+        
+        return 
+        
+        
+        
