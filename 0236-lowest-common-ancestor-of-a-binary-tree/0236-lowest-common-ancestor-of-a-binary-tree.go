ea079b8a0ffa15b5e326d2eb6101d14511644eee
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-11 23:54:41 -0500

    Time: 6 ms (95.17%), Space: 7.6 MB (75.53%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.go
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.go
@@ -8,24 +8,19 @@
  */
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
     if root == p || root == q {
         return root
     }
-
-    var left *TreeNode
-    if root.Left != nil {
-        left = lowestCommonAncestor(root.Left, p, q)
-    }
-
-    var right *TreeNode
-    if root.Right != nil {
-        right = lowestCommonAncestor(root.Right, p, q)
-    }
-
+    
+    left := lowestCommonAncestor(root.Left, p, q)
+    right := lowestCommonAncestor(root.Right, p, q)
+    
     if left != nil && right != nil {
         return root
     }
-    
     if left != nil {
         return left
     }
