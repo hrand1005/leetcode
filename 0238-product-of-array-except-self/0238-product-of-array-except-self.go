0cef3413f6e9c01cab6506864011d72be2247295
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-12 19:16:19 -0500

    Time: 18 ms (99.44%), Space: 7.6 MB (54.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,3 +1,4 @@
+/*
 func productExceptSelf(nums []int) []int {
     n := len(nums)
     
@@ -19,6 +20,27 @@ func productExceptSelf(nums []int) []int {
     for i := 1; i < n-1; i++ {
         result[i] = forward[i-1] * backward[i+1]
     }
+    
+    return result
+}
+*/
+
+func productExceptSelf(nums []int) []int {
+    n := len(nums)
+    result := make([]int, n)
+    
+    product := 1
+    for i := 0; i < n; i++ {
+        result[i] = product * nums[i]
+        product *= nums[i]
+    }
+    
+    product = 1
+    for i := n-1; i > 0; i-- {
+        result[i] = product * result[i-1]
+        product *= nums[i]
+    }
+    result[0] = product
     
     return result
 }
