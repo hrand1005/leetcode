4e45272dfb17cdac7cfede07548cb1db6eec16e4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-12 19:05:12 -0500

    Time: 241 ms (81.70%), Space: 21.2 MB (90.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         forward = [0] * len(nums)
@@ -15,5 +16,22 @@ class Solution:
         result[-1] = forward[-2]
         for i in range(1, len(nums)-1):
             result[i] = forward[i-1] * backward[i+1]
+        
+        return result
+"""
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [0] * len(nums)
+        product = 1
+        for i in range(0, len(nums)):
+            result[i] = product * nums[i]
+            product = result[i]
+        
+        product = 1
+        for i in range(len(nums)-1, 0, -1):
+            result[i] = product * result[i-1]
+            product *= nums[i]
+        result[0] = product
         
         return result
