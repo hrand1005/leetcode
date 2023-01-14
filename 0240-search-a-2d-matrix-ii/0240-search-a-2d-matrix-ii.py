39c36f5e84b3d9c6751e8bad067d803f531bec0f
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-14 11:54:05 -0500

    Time: 172 ms (88.85%), Space: 20.4 MB (83.03%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,22 +1,4 @@
 """
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in range(len(matrix)):
-            if self.found(matrix[row], target, 0, len(matrix[row])-1):
-                return True
-        return False    
-    
-    def found(self, row: int, target: int, low: int, high: int) -> bool:
-        if high < low:
-            return False
-        midpoint = (low + high) // 2
-        if row[midpoint] == target:
-            return True
-        if row[midpoint] < target:
-            return self.found(row, target, midpoint+1, high)
-        if target < row[midpoint]:
-            return self.found(row, target, low, midpoint-1)
-"""
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
@@ -24,3 +6,15 @@ class Solution:
             if target in row:
                 return True
         return False    
+"""
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row, col = 0, len(matrix[0])-1
+        while row <= len(matrix)-1 and 0 <= col:
+            if matrix[row][col] == target:
+                return True
+            elif target < matrix[row][col]:
+                col -= 1
+            else:
+                row += 1
+        return False        
