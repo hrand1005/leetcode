bee20b9bf1480b024bb0206a50c4c7e6f9ff0382
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-14 12:11:59 -0500

    Time: 28 ms (61.04%), Space: 6.8 MB (58.44%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.go
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.go
@@ -1,3 +1,4 @@
+/*
 func searchMatrix(matrix [][]int, target int) bool {
     row, col := 0, len(matrix[0])-1
     for row <= len(matrix)-1 && 0 <= col {
@@ -10,4 +11,31 @@ func searchMatrix(matrix [][]int, target int) bool {
         }
     }
     return false
+}
+*/
+
+func searchMatrix(matrix [][]int, target int) bool {
+    if len(matrix) == 0 || len(matrix[0]) == 0 {
+        return false
+    }
+    
+    topRight := matrix[0][len(matrix[0])-1]
+    
+    if topRight == target {
+        return true
+    }
+    
+    if topRight < target {
+        return searchMatrix(matrix[1:], target)
+    }
+    
+    return searchMatrix(removeRightCol(matrix), target)
+}
+
+func removeRightCol(matrix [][]int) [][]int {
+    newMatrix := make([][]int, len(matrix))
+    for i, row := range matrix {
+        newMatrix[i] = row[:len(row)-1]
+    }
+    return newMatrix
 }
