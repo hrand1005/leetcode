05eee8b40d54bc0fea7360da256de1259a4a0506
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-15 19:39:02 -0500

    Time: 2 ms (54.59%), Space: 2 MB (49.26%) - LeetHub

--- 0027-remove-element/0027-remove-element.go
+++ 0027-remove-element/0027-remove-element.go
@@ -1,3 +1,4 @@
+/*
 func removeElement(nums []int, val int) int {
     left, right := 0, len(nums)-1
     for left <= right {
@@ -9,4 +10,16 @@ func removeElement(nums []int, val int) int {
         }
     }
     return left
+}
+*/
+
+func removeElement(nums []int, val int) int {
+    i := 0
+    for _, n := range nums {
+        if n != val {
+            nums[i] = n
+            i++
+        }
+    }
+    return i
 }
