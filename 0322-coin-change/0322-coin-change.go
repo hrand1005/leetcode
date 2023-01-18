5a62248cbbebf6c1966b22f0aff3421b45a15656
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-17 19:29:06 -0500

    Time: 180 ms (27.36%), Space: 8.1 MB (17.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.go
+++ 0322-coin-change/0322-coin-change.go
@@ -1,3 +1,4 @@
+/*
 import (
     "math"
 )
@@ -17,6 +18,42 @@ func coinChange(coins []int, amount int) int {
         return -1
     }
     return table[amount]
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+*/
+
+func coinChange(coins []int, amount int) int {
+    cache := make(map[int]int)
+    
+    var coinChangeRecursive func([]int, int) int
+    coinChangeRecursive = func(coins []int, amount int) int {
+        if v, ok := cache[amount]; ok {
+            return v
+        }
+        if amount == 0 {
+            return 0
+        }
+        minCoins := math.MaxInt32
+        for _, c := range coins {
+            if amount-c >= 0 {
+                minCoins = min(minCoins, 1+coinChangeRecursive(coins, amount-c))
+            }
+        }
+        cache[amount] = minCoins
+        return minCoins
+    }
+    
+    res := coinChangeRecursive(coins, amount)
+    if res == math.MaxInt32 {
+        return -1
+    }
+    return res
 }
 
 func min(a, b int) int {
