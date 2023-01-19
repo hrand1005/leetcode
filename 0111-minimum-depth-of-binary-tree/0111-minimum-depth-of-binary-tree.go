8eaa63b073a6f79c2c66fc0e1223ff088d613ff8
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-18 23:17:43 -0500

    Time: 188 ms (81.36%), Space: 18.9 MB (68.77%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.go
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type NodeWithLevel struct {
+    *TreeNode
+    Level int
+}
+
+func minDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    queue := []*NodeWithLevel{
+        {
+            TreeNode: root,
+            Level: 1,
+        },
+    }
+    for len(queue) != 0 {
+        node := queue[0]
+        queue = queue[1:]
+        if node.Left == nil && node.Right == nil {
+            return node.Level
+        }
+        if node.Left != nil {
+            queue = append(queue, &NodeWithLevel{
+                TreeNode: node.Left,
+                Level: node.Level + 1,
+            })
+        }
+        if node.Right != nil {
+            queue = append(queue, &NodeWithLevel{
+                TreeNode: node.Right,
+                Level: node.Level + 1,
+            })
+        }
+    }
+    return -1
+}
