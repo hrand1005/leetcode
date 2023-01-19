419774b258f473b9cd7a431037496bf7d6f182b4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-18 22:57:43 -0500

    Time: 0 ms (100.00%), Space: 3.2 MB (100.00%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.go
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.go
@@ -5,35 +5,6 @@
  *     Next *ListNode
  * }
  */
-/*
-func oddEvenList(head *ListNode) *ListNode {
-    if head == nil {
-        return head
-    }
-    
-    even := head
-    oddStart := head.Next
-    odd := oddStart
-    for even != nil && odd != nil {
-        if even.Next != nil {
-            even.Next = even.Next.Next
-            even = even.Next
-        }
-        if odd.Next != nil {
-            odd.Next = odd.Next.Next
-            odd = odd.Next
-        }
-    }
-    
-    current := head
-    for current.Next != nil {
-        current = current.Next
-    }
-    current.Next = oddStart
-    return head
-}
-*/
-
 func oddEvenList(head *ListNode) *ListNode {
     if head == nil {
         return head
