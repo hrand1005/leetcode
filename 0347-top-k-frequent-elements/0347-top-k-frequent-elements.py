10c1c2dc89dc7f13bcd8798eb7fd94df1cb07b2d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-20 18:21:41 -0500

    Time: 101 ms (91.63%), Space: 18.7 MB (46.13%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,20 +4,20 @@ class Solution:
         for n in nums:
             freq[n] = freq.get(n, 0) + 1
         
-        frequencies = []
+        # frequencies = []
         freq_to_val = {}
         for num, freq in freq.items():
-            frequencies.append(freq)
+            # frequencies.append(freq)
             freq_to_val[freq] = freq_to_val.get(freq, []) + [num]
         
-        frequencies.sort()
-        top_k_freqs = set(frequencies[-k:])
-        
-        result = []
-        for n in top_k_freqs:
-            result += freq_to_val[n]
-        
-        return result
+        frequencies = []
+        idx = len(nums)
+        while len(frequencies) < k:
+            if freq_to_val.get(idx) != None:
+                frequencies += freq_to_val[idx]
+            idx -= 1
+            
+        return frequencies
         
             
             
