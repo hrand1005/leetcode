79aa71e9c28b4a46b6517b2c31013d631b42ff77
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-21 01:08:59 -0500

    Time: 1 ms (69.39%), Space: 1.8 MB (100.00%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.go
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.go
@@ -1,6 +1,15 @@
+/*
 func getSum(a int, b int) int {
     if b == 0 {
         return a
     }
     return getSum(a^b, (a&b)<<1)
+}
+*/
+
+func getSum(a int, b int) int {
+    for b != 0 {
+        a, b = (a^b), (a&b)<<1
+    }
+    return a
 }
