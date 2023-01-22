30fa4f40e40023d92e4eaa4676f47b11472c7189
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-22 02:03:27 -0500

    Time: 2 ms (26.55%), Space: 2.2 MB (15.38%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.go
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.go
@@ -6,6 +6,7 @@
  *     Right *TreeNode
  * }
  */
+/*
 func postorderTraversal(root *TreeNode) []int {
     if root == nil {
         return nil
@@ -13,4 +14,21 @@ func postorderTraversal(root *TreeNode) []int {
     left := postorderTraversal(root.Left)
     right := postorderTraversal(root.Right)
     return append(left, append(right, root.Val)...)
+}
+*/
+
+func postorderTraversal(root *TreeNode) []int {
+    stack := []*TreeNode{root}
+    postorder := []int{}
+    for len(stack) != 0 {
+        node := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        if node == nil {
+            continue
+        }
+        postorder = append([]int{node.Val}, postorder...)
+        stack = append(stack, node.Left)
+        stack = append(stack, node.Right)
+    }
+    return postorder
 }
