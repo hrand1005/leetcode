97a39002d7b8f415b0b32e6fe144e6348623321e
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-01-22 01:48:35 -0500

    Time: 0 ms (100.00%), Space: 2.1 MB (9.01%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
@@ -6,6 +6,7 @@
  *     Right *TreeNode
  * }
  */
+/*
 func preorderTraversal(root *TreeNode) []int {
     if root == nil {
         return nil
@@ -14,4 +15,21 @@ func preorderTraversal(root *TreeNode) []int {
     left := preorderTraversal(root.Left)
     right := preorderTraversal(root.Right)
     return append(preorder, append(left, right...)...)
+}
+*/
+
+func preorderTraversal(root *TreeNode) []int {
+    stack := []*TreeNode{root}
+    preorder := []int{}
+    for len(stack) != 0 {
+        node := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        if node == nil {
+            continue
+        }
+        preorder = append(preorder, node.Val)
+        stack = append(stack, node.Right)
+        stack = append(stack, node.Left)
+    }
+    return preorder
 }
