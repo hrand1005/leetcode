ae2f17c62281cf07ba793a303fbeebc37b52a879
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-01 21:03:30 -0500

    Time: 16 ms (66.29%), Space: 5 MB (83.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
@@ -6,14 +6,13 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
     if len(nums2) < len(nums1) {
         nums1, nums2 = nums2, nums1
     }
-    fmt.Println(nums1)
     len1, len2 := len(nums1), len(nums2)
-    full := len1 + len2
+    size := len1 + len2
     
     low, high := 0, len1
     for low <= high {
         mid1 := (low + high) / 2
-        mid2 := full / 2 - mid1
+        mid2 := size / 2 - mid1
         
         left1, right1 := math.MinInt32, math.MaxInt32
         if 0 < mid1 {
@@ -31,7 +30,7 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
             right2 = nums2[mid2]
         }
         
-        if med, ok := median(left1, right1, left2, right2, full); ok {
+        if med, ok := median(left1, right1, left2, right2, size); ok {
             return med
         } else if right2 < left1 {
             high = mid1 - 1
@@ -43,9 +42,9 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
     return 0
 }
 
-func median(l1, r1, l2, r2, full int) (float64, bool) {
+func median(l1, r1, l2, r2, size int) (float64, bool) {
     if l1 <= r2 && l2 <= r1 {
-        if full % 2 == 0 {
+        if size % 2 == 0 {
             return float64((max(l1, l2) + min(r1, r2))) / 2, true
         }
         return float64(min(r1, r2)), true
