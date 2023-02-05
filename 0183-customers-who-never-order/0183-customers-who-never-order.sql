72b34bff39619c4183b44a00421e97646f100f49
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-05 13:53:07 -0500

    Time: 1176 ms (11.88%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -4,7 +4,11 @@
 # LEFT JOIN Orders ON Customers.id = Orders.customerId
 # WHERE customerId IS NULL;
 
-SELECT A.name AS 'Customers'
-FROM Customers AS A
-LEFT JOIN Orders AS B ON A.id = B.customerId
-WHERE B.customerId IS NULL;
+# SELECT A.name AS 'Customers'
+# FROM Customers AS A
+# LEFT JOIN Orders AS B ON A.id = B.customerId
+# WHERE B.customerId IS NULL;
+
+SELECT name AS 'Customers'
+FROM Customers
+WHERE NOT EXISTS (SELECT * FROM Orders WHERE Orders.customerId = Customers.Id LIMIT 1);
