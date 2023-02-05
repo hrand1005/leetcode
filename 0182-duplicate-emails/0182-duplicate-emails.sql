3d389818c91d62a1468a5728618bfe1845eb833d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-05 13:38:43 -0500

    Time: 635 ms (45.21%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
-WITH email_count AS (
-    SELECT Email, COUNT(Email) AS 'Count'
-    FROM Person
-    GROUP BY Email
-)
-SELECT DISTINCT person.Email
+# WITH email_count AS (
+#     SELECT Email, COUNT(Email) AS 'Count'
+#     FROM Person
+#     GROUP BY Email
+# )
+# SELECT DISTINCT person.Email
+# FROM Person
+# JOIN email_count ON person.Email = email_count.Email
+# WHERE email_count.Count > 1;
+
+SELECT Email
 FROM Person
-JOIN email_count ON person.Email = email_count.Email
-WHERE email_count.Count > 1;
+GROUP BY Email
+HAVING COUNT(*) > 1;
