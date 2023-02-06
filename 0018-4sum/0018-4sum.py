853b1944833eade026ef8dcb0a9fde7a1d2693ca
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-06 18:32:20 -0500

    Time: 1326 ms (35.35%), Space: 17.1 MB (5.17%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -20,29 +20,27 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         quads = set()
-        
         subproblems = {}
         for i in range(len(nums)):
             for j in range(i+3, len(nums)):
                 subproblems[(i,j)] = nums[i] + nums[j]
-        
+                
         for k, v in subproblems.items():
             start, end = k[0]+1, k[1]
             sums = self.two_sum(nums[start:end], target-v)
             for s in sums:
                 quads.add((nums[k[0]], s[0], s[1], nums[k[1]]))
-        
+                
         result = []
         for q in quads:
             result.append(list(q))
         return result    
         
     def two_sum(self, nums: List[int], target: int) -> Set[Tuple[int]]:
-        res = set()
-        occ = {}
+        res, occ = set(), set()
         for i in range(len(nums)):
             comp = target - nums[i]
-            if occ.get(comp) != None:
+            if comp in occ:
                 res.add((comp, nums[i]))
-            occ[nums[i]] = i
+            occ.add(nums[i])
         return res    
