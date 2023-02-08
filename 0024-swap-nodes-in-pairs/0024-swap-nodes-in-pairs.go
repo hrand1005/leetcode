db3fede4dfee4dda2bb7d7f20bb88a538024275a
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-07 23:48:37 -0500

    Time: 1 ms (65.70%), Space: 2 MB (100.00%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.go
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.go
@@ -9,9 +9,34 @@ func swapPairs(head *ListNode) *ListNode {
     if head == nil || head.Next == nil {
         return head
     }
+    
+    var prev *ListNode
+    cur := head
+    newHead := head.Next
+    
+    for cur != nil && cur.Next != nil {
+        p1, p2 := cur, cur.Next
+        cur = cur.Next.Next
+        p2.Next = p1
+        p1.Next = cur
+        if prev != nil {
+            prev.Next = p2  
+        }
+        prev = p1
+    }
+    
+    return newHead
+}
+    
+/*    
+func swapPairs(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
     next := head.Next.Next
     p1, p2 := head, head.Next
     p2.Next = p1
     p1.Next = swapPairs(next)
     return p2
 }
+*/
