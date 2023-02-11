8702f60853266e68d062acfb00695d6d6045f43b
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-10 22:39:43 -0500

    Time: 58 ms (82.29%), Space: 14 MB (59.69%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        
+        combos = []
+        for i in range(len(candidates)):
+            if candidates[i] == target:
+                combos.append([candidates[i]])
+            if candidates[i] < target:
+                subcombos = self.combinationSum(candidates[i:], target-candidates[i])
+                combos += [[candidates[i]] + s for s in subcombos]
+        
+        return combos
