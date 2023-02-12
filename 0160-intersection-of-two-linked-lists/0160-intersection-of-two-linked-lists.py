947d134f3c484c9fb2a5a7c6f82950123a0a7be5
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-12 15:51:13 -0500

    Time: 188 ms (39.85%), Space: 43.8 MB (16.97%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,47 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-"""
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        if headA == headB:
-            return headA
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        visited = {}
+        while headA != None:
+            visited[headA] = True
+            headA = headA.next
         
-        encountered_nodes = {}
-        cur1 = headA
-        cur2 = headB
-        while cur1 != None or cur2 != None:
-            if cur1:
-                if encountered_nodes.get(cur1):
-                    return cur1
-                encountered_nodes[cur1] = 1
-                cur1 = cur1.next
-                
-            if cur2:
-                if encountered_nodes.get(cur2):
-                    return cur2
-                encountered_nodes[cur2] = 1
-                cur2 = cur2.next
+        while headB != None:
+            if visited.get(headB, False):
+                return headB
+            headB = headB.next
+            
+        return None    
         
-        return None
-"""
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        cur1 = headA
-        cur2 = headB
         
-        while cur1 != cur2:
-            if cur1 == None:
-                cur1 = headB
-            else:
-                cur1 = cur1.next
-                
-            if cur2 == None:
-                cur2 = headA
-            else:
-                cur2 = cur2.next
-                
-        return cur1
