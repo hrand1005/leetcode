400866b5e7046cb3caa55aa96b300440a8a229bb
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-12 11:22:38 -0500

    Time: 1 ms (68.35%), Space: 1.9 MB (33.54%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.go
@@ -0,0 +1,53 @@
+type MyStack struct {
+    q1 []int
+    q2 []int
+    top int
+}
+
+
+func Constructor() MyStack {
+    return MyStack{
+        q1: make([]int, 0),
+        q2: make([]int, 0),
+    }
+}
+
+
+func (this *MyStack) Push(x int)  {
+    this.q1 = append(this.q1, x)
+    this.top = x
+}
+
+
+func (this *MyStack) Pop() int {
+    n := len(this.q1)
+    for i := 0; i < n-1; i++ {
+        this.top = this.q1[0]
+        this.q1 = this.q1[1:]
+        this.q2 = append(this.q2, this.top)
+    }
+    pop := this.q1[0]
+    this.q1 = this.q1[1:]
+    this.q1, this.q2 = this.q2, this.q1
+    return pop
+}
+
+
+func (this *MyStack) Top() int {
+    return this.top
+}
+
+
+func (this *MyStack) Empty() bool {
+    return len(this.q1) == 0
+}
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
