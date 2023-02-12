c8a1c8c23e1ab0dae26c2c0c24cfc1dec9c86744
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-12 14:15:02 -0500

    Time: 108 ms (63.83%), Space: 15.8 MB (52.40%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,23 +1,7 @@
-"""
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        occurs = {}
-        for n in nums:
-            if occurs.get(n) == None:
-                occurs[n] = 1
-            else:   
-                occurs[n] += 1
-        
-        for k, v in occurs.items():
-            if v == 1:
-                return k
-            
-        return None
-"""
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+class Solution(object):
+    def singleNumber(self, nums):
         res = 0
         for n in nums:
             res ^= n
-            
         return res    
+        
