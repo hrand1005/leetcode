911a95240e0cc0698ca7887e51652e78ca7dca76
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-12 13:47:12 -0500

    Time: 29 ms (32.73%), Space: 13.8 MB (5.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,20 +1,30 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        if n == 0:
-            return 
+        idx = len(nums1)-1
+        l, r = len(nums1)-len(nums2)-1, len(nums2)-1
         
-        write_index = m + n - 1
-        i = m - 1
-        j = n - 1
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[write_index] = nums1[i]
-                i -= 1
+        while idx >= 0:
+            if l >= 0 and r >= 0:
+                if nums1[l] > nums2[r]:
+                    nums1[idx] = nums1[l]
+                    l -= 1
+                else:
+                    nums1[idx] = nums2[r]
+                    r -= 1
+            elif l < 0:
+                nums1[idx] = nums2[r]
+                r -= 1
             else:
-                nums1[write_index] = nums2[j]
-                j -= 1
-            write_index -= 1
+                nums1[idx] = nums1[l]
+                l -= 1
+            idx -= 1    
             
+        return    
+        
