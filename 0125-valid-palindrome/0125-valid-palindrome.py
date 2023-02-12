7e29d57a8689a5b56400f9ef82a7c15e811e1608
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-12 14:07:26 -0500

    Time: 50 ms (52.06%), Space: 14.1 MB (93.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,47 +1,31 @@
 """
-VALID_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789'
-
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = self.sanitizeInput(s)
-        
-        for i in range(len(s)):
-            if s[i] != s[len(s)-1-i]:
+class Solution(object):
+    def isPalindrome(self, s):
+        if len(s) <= 1:
+            return True
+        first, last = s[0], s[-1]
+        if first.isalnum() and last.isalnum():
+            if lower(first) != lower(last):
                 return False
-        
-        return True
-    
-    def sanitizeInput(self, s: str) -> str:
-        s = s.lower()
-        
-        i = 0
-        while i < len(s):
-            if s[i] not in VALID_CHARS:
-                char = s[i]
-                s = s[0:i] + s[i+1:]
-            else:
-                i += 1
-        
-        return s
+            return self.isPalindrome(s[1:-1])
+        elif not first.isalnum() and not last.isalnum():
+            return self.isPalindrome(s[1:-1])
+        elif not first.isalnum():
+            return self.isPalindrome(s[1:])
+        else:
+            return self.isPalindrome(s[:-1])
 """
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        i = 0
-        j = len(s) - 1
-        while i < j:    
-            while i < j and not s[i].isalnum():
-                i += 1
-                
-            while i < j and not s[j].isalnum():
-                j -= 1
-            
-            if i >= j:
-                return True
-            
-            if s[i].lower() != s[j].lower():
-                return False
-                
-            i += 1
-            j -= 1
 
-        return True       
+class Solution(object):
+    def isPalindrome(self, s):
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if l < r and s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
