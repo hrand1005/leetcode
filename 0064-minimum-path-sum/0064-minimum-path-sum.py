b6940e7536f29963914039ac14e8ec3cf69a4cc3
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-12 22:56:11 -0500

    Time: 111 ms (46.00%), Space: 15.7 MB (82.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         MAX_INT = 2 ** 32
@@ -17,4 +18,22 @@ class Solution:
                     from_left = table[i][j-1] + grid[i][j-1]
                     table[i][j] = min(from_top, from_left)
                     
+        return table[-1][-1] + grid[-1][-1]
+"""
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        MAX_INT = 2 ** 32
+        table = [[MAX_INT] * len(grid[i]) for i in range(len(grid))]
+        table[0][0] = 0
+        
+        for i in range(1, len(grid)):
+            table[i][0] = table[i-1][0] + grid[i-1][0]
+        for j in range(1, len(grid[0])):
+            table[0][j] = table[0][j-1] + grid[0][j-1]
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[i])):
+                top = table[i-1][j] + grid[i-1][j]
+                left = table[i][j-1] + grid[i][j-1]
+                table[i][j] = min(top, left)
+                
         return table[-1][-1] + grid[-1][-1]
