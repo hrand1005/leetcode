cbb9808015158ff6aa2330f813678054ecf803cc
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-12 19:31:12 -0500

    Time: 579 ms (5.47%), Space: 8 MB (5.48%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.go
+++ 0045-jump-game-ii/0045-jump-game-ii.go
@@ -1,4 +1,39 @@
 func jump(nums []int) int {
+    cache := make(map[int]int, len(nums))
+    
+    var jumpRec func([]int, int) int
+    jumpRec = func(nums []int, idx int) int {
+        if v, ok := cache[idx]; ok {
+            return v
+        }
+        if idx == len(nums)-1 {
+            return 0
+        }
+        scope := nums[idx]
+        if idx + scope >= len(nums)-1 {
+            return 1
+        }
+        minJumps := math.MaxInt32
+        for i := idx+1; i <= idx + scope; i++ {
+            minJumps = min(minJumps, jumpRec(nums, i))
+        }
+        minJumps++
+        cache[idx] = minJumps
+        return minJumps
+    }
+    
+    return jumpRec(nums, 0)
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+/*
+func jump(nums []int) int {
     if len(nums) <= 1 {
         return 0
     }
@@ -21,3 +56,4 @@ func max(a, b int) int {
     }
     return a
 }
+*/
