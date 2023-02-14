7b5a90117719ac00c5e51959c01b8b4cb5b39bc2
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-13 19:24:28 -0500

    Time: 48 ms (57.70%), Space: 14.4 MB (83.27%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,3 +1,4 @@
+"""
 NOT_FOUND = 2 ** 32
 
 class Solution:
@@ -33,3 +34,21 @@ class Solution:
             else:
                 high = mid - 1
         return NOT_FOUND        
+"""
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        low, high = 0, (m * n) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            row = mid // n
+            col = mid % n
+            
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
