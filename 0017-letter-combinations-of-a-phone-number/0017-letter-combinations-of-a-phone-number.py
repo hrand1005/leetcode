ef1b7e01883d93bda6c2d4119afedad7ed1c28d2
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-13 21:41:26 -0500

    Time: 11 ms (96.80%), Space: 13.5 MB (47.37%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,45 +1,29 @@
-NUM_MAP = {
-    "2": "abc",
-    "3": "def",
-    "4": "ghi",
-    "5": "jkl",
-    "6": "mno",
-    "7": "pqrs",
-    "8": "tuv",
-    "9": "wxyz",
-}
-
-"""
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        num_to_letter = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        
         if len(digits) == 0:
             return []
-        
-        combos = {}
-        combos[0] = list(NUM_MAP[digits[0]])
-        for i in range(1, len(digits)):
-            combos[i] = []
-            for combo in combos[i-1]:
-                for letter in list(NUM_MAP[digits[i]]):
-                    combos[i].append(combo + letter)
-                    
-        return combos[len(digits)-1]
-"""
-
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return []
-        
-        this = list(NUM_MAP[digits[-1]])
-        
         if len(digits) == 1:
-            return this
+            return list(num_to_letter[digits])
         
+        this = list(num_to_letter[digits[0]])
+        rest = self.letterCombinations(digits[1:])
         combos = []
-        prev = self.letterCombinations(digits[:-1])
-        for letter in prev:
-            for c in this:
-                combos.append(letter + c)
-        
-        return combos
+        for t in this:
+            for r in rest:
+                combos.append(t+r)
+        return combos    
