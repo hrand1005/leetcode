e2762160cea1a6a50b560e9d4c304c93143fa9c0
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-14 21:42:17 -0500

    Time: 14 ms (95.37%), Space: 13.3 MB (68.79%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,50 +1,18 @@
-"""
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        occurred = {}
-        
-        while occurred.get(n) == None:
-            occurred[n] = True
-            num_string = str(n)
-            this_sum = 0
-            
-            for digit in num_string:
-                this_sum += int(digit) ** 2
-        
-            if this_sum == 1:
-                return True
-            
-            n = this_sum
-        
-        return False
-"""
-
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        slow = n
-        fast = self.sum_squared_digits(slow)
-        
-        while slow != fast and fast != 1:
-            slow = self.sum_squared_digits(slow)
-            fast = self.sum_squared_digits(fast)
-            
-            # not really necessary to check if fast == 1
-            # because it will remain 1 in perpetuity
-            if fast == slow:
+class Solution(object):
+    def isHappy(self, n):
+        seen = {}
+        dsum = self.digit_sum(n)
+        seen[dsum] = True
+        while dsum != 1:
+            dsum = self.digit_sum(dsum)
+            if seen.get(dsum, False):
                 return False
-            
-            # go again for fast!
-            fast = self.sum_squared_digits(fast)
-            
-        if fast == 1:
-            return True
+            seen[dsum] = True
+        return True    
+    
+    def digit_sum(self, num):
+        total = 0
+        for c in str(num):
+            total += int(c) ** 2
+        return total    
         
-        return False    
-        
-    def sum_squared_digits(self, n: int) -> int:
-        digit_sum = 0
-        n_string = str(n)
-        for digit in n_string:
-            digit_sum += int(digit) ** 2
-            
-        return digit_sum    
