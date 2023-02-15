b81f54bd98c8dd479b572021d39b1584397db26b
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-14 21:25:44 -0500

    Time: 26 ms (34.98%), Space: 13.5 MB (66.15%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,72 +1,15 @@
-"""
-POS_MAP = { 
-    'A': 1,
-    'B': 2,
-    'C': 3,
-    'D': 4,
-    'E': 5,
-    'F': 6,
-    'G': 7,
-    'H': 8,
-    'I': 9,
-    'J': 10,
-    'K': 11,
-    'L': 12,
-    'M': 13,
-    'N': 14,
-    'O': 15,
-    'P': 16,
-    'Q': 17,
-    'R': 18,
-    'S': 19,
-    'T': 20,
-    'U': 21,
-    'V': 22,
-    'W': 23,
-    'X': 24,
-    'Y': 25,
-    'Z': 26,
-}
-
-class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        return self.recursive(0, columnTitle)
-    
-    def recursive(self, multiplier, columnTitle) -> int:
-        if len(columnTitle) == 0:
-            return 0
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        col_val = {}
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for i in range(26):
+            col_val[alphabet[i]] = i + 1
         
-        last = POS_MAP[columnTitle[len(columnTitle)-1]]
-        return 26 ** multiplier * last + self.recursive(multiplier + 1, columnTitle[:-1])
-
-
-class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        col = 0
-        i = len(columnTitle) - 1
-        j = 0
-        while i >= 0:
-            last_char = columnTitle[i]
-            col += 26 ** j * POS_MAP[last_char]
-            i -= 1
-            j += 1
+        total = col_val[columnTitle[0]]
+        for c in columnTitle[1:]:
+            total = total * 26 + col_val[c]
         
-        return col
-        
-"""
-
-# Ord converts unicode characters to ints. ord('A') --> 65, 
-# thus we can calculate the column with ord('A') - 64 --> 1
-class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        col = 0
-        i = len(columnTitle) - 1
-        j = 0
-        while i >= 0:
-            last_char = columnTitle[i]
-            col += 26 ** j * (ord(last_char) - 64) 
-            i -= 1
-            j += 1
-        
-        return col
+        return total
+            
+            
         
