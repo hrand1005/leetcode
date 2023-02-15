54e29c9883dd24dbc83a37e0bbc7383b5c5e6afa
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-14 21:35:46 -0500

    Time: 25 ms (34.84%), Space: 13.3 MB (89.34%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -5,7 +5,7 @@ class Solution:
         res = 0
         for i in range(32):
             res <<= 1
-            res += n % 2
+            res |= n % 2
             n >>= 1
         return res    
         
