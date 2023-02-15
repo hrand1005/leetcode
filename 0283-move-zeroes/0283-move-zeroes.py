1f3cf23cf9b30ae41a6e4ff2df4120419bedd373
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-14 21:57:21 -0500

    Time: 134 ms (69.47%), Space: 14.7 MB (68.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,34 +1,11 @@
-"""
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        i, j = 0, 0
-        while True:
-            while nums[i] != 0:
-                i += 1
-                if i == len(nums):
-                    return
-
-            j = i
-
-            while nums[j] == 0:
-                j += 1
-                if j == len(nums):
-                    return
-
-            nums[i], nums[j] = nums[j], nums[i]
-
-            i += 1
-"""
-
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        i, j = 0, 0
-        while j < len(nums):
-            if nums[i] == 0 and nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-
+class Solution(object):
+    def moveZeroes(self, nums):
+        zero = 0
+        for i in range(len(nums)):
             if nums[i] != 0:
-                i += 1
-
-            j += 1
-
+                nums[zero], nums[i] = nums[i], nums[zero]
+                zero += 1
+                
+                
+            
+        
