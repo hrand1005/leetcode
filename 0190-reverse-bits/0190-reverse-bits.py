1f9397404748b58d7e831fcfa5469ad04d1d1b1f
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-02-14 21:35:16 -0500

    Time: 23 ms (46.27%), Space: 13.3 MB (89.34%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,24 +1,11 @@
-"""
 class Solution:
-    def reverseBits(self, n: int) -> int:
-        # convert to string, remove 0b from beginning
-        s = bin(n)[2:]
-        # reverse string
-        s = s[::-1]
-        # pad with zeroes up to 32 to the end -- remember, these
-        # zeroes occurred at the beginning in the initial integer
-        s = s + "0"*(32-len(s))
-        # convert back to integer
-        s = int(s, 2)
-        return s
-"""
-
-class Solution:
-    def reverseBits(self, n: int) -> int:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
         res = 0
         for i in range(32):
-            res = res << 1 # left shift res
-            res += n%2 # adds 1 if n's least significant bit is 1
-            n = n >> 1 # right shift n
+            res <<= 1
+            res += n % 2
+            n >>= 1
+        return res    
         
-        return res
