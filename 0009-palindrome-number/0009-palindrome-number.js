5055fba9c1ddbf08d21cadfb8075e2a7da7ec0f5
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-03-13 01:33:27 -0400

    Time: 176 ms (66.78%), Space: 50.5 MB (80.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,6 +2,7 @@
  * @param {number} x
  * @return {boolean}
  */
+/*
 let isPalindrome = function(x) {
     const numString = x.toString();
     for (let i = 0; i < numString.length/2; i++) {
@@ -11,3 +12,18 @@ let isPalindrome = function(x) {
     }
     return true;
 };
+*/
+
+let isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+    let original = x;
+    let reverse = 0;
+    while (x != 0) {
+        reverse *= 10;
+        reverse += x % 10;
+        x = Math.floor(x/10);
+    }
+    return reverse == original;
+}
