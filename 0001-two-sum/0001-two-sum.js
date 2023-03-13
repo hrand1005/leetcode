e816ca98b3ed108f67c47a6c77e35145c4625b84
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-03-13 01:11:58 -0400

    Time: 56 ms (97.89%), Space: 43 MB (29.89%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const valToIdx = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (valToIdx.has(complement)) {
+            return [valToIdx.get(complement), i];
+        }
+        valToIdx.set(nums[i], i);
+    }
+    return null;
+};
