9642cc752934ec2feb432083161b6aff2d1fe7f5
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-03 13:21:56 -0400

    Time: 1 ms (70.18%), Space: 2 MB (48.25%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.go
@@ -0,0 +1,30 @@
+func summaryRanges(nums []int) []string {
+    if len(nums) < 1 {
+        return nil
+    }
+    ranges := make([][]int, 0, len(nums))
+    start := nums[0]
+    current := []int{ start, start }
+    for i := 1; i < len(nums); i++ {
+        if nums[i] == nums[i-1] + 1 {
+            current[1] = nums[i]
+        } else {
+            ranges = append(ranges, current)
+            current = []int{nums[i], nums[i]}
+        }
+    }
+    ranges = append(ranges, current)
+    return postProcess(ranges)
+}
+
+func postProcess(ranges [][]int) []string {
+    rangeStrings := make([]string, len(ranges))
+    for i := 0; i < len(rangeStrings); i++ {
+        if ranges[i][0] == ranges[i][1] {
+            rangeStrings[i] = fmt.Sprintf("%v", ranges[i][0])
+        } else {
+            rangeStrings[i] = fmt.Sprintf("%v->%v", ranges[i][0], ranges[i][1])
+        }
+    }
+    return rangeStrings
+}
