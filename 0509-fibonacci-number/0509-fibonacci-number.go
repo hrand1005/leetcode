88aca772f963cd14570f8771ec35de02c91d830d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-04 20:24:13 -0400

    Time: 0 ms (100.00%), Space: 1.9 MB (84.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.go
+++ 0509-fibonacci-number/0509-fibonacci-number.go
@@ -28,8 +28,8 @@ func fib(n int) int {
         return n
     }
     twoBefore, oneBefore := 0, 1
-    for i := 0; i < n-1; i++ {
+    for i := 2; i < n; i++ {
         twoBefore, oneBefore = oneBefore, oneBefore + twoBefore
     }
-    return oneBefore
+    return twoBefore + oneBefore
 }
