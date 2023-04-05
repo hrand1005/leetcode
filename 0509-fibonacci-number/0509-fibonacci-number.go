05fdc368a6e3a81d82197306ee066f9cc1611406
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-04 20:19:51 -0400

    Time: 296 ms (5.33%), Space: 2.1 MB (22.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.go
+++ 0509-fibonacci-number/0509-fibonacci-number.go
@@ -1,6 +1,26 @@
+/*
 func fib(n int) int {
     if n <= 1 {
         return n
     }
     return fib(n-1) + fib(n-2)
+}
+*/
+
+func fib(n int) int {
+    memo := map[int]int{
+        0: 0,
+        1: 1,
+    }
+    
+    var fibRecurse func(int) int
+    fibRecurse = func(n int) int {
+        if v, ok := memo[n]; ok {
+            return v
+        }
+        memo[n] = fib(n-1) + fib(n-2)
+        return memo[n]
+    }
+    
+    return fibRecurse(n)
 }
