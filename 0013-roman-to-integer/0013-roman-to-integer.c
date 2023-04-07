9c7cb32511bc9b03d292716d54f9cd046caa299c
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-07 16:55:57 -0400

    Time: 28 ms (6.40%), Space: 5.7 MB (88.91%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -0,0 +1,38 @@
+int convert_numeral(char c) {
+    switch(c) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+    }
+    return -1;
+}
+
+int romanToInt(char * s){
+    int s_len = strlen(s);
+    int total = 0;
+    int last = convert_numeral(s[0]);
+    
+    for (int i = 0; i < s_len; i++) {
+        printf("processing %c\n", s[i]);
+        int val = convert_numeral(s[i]);
+        if (val > last) {
+            total -= 2 * last;
+        } 
+        total += val;
+        last = val;
+        printf("total: %d\n", total);
+    }
+    
+    return total;
+}
