03d175f35e7c31582fdeff4192a3548a2b20f1ab
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-07 01:08:07 -0400

    Time: 136 ms (51.26%), Space: 6.6 MB (10.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -4,6 +4,7 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     *returnSize = 2;
     int* result = malloc(sizeof(int)*(*returnSize));
+    
     for (int i = 0; i < numsSize; i++) {
         for (int j = i+1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
