1c833f8de4a2828c1cbd25e7f2e9f084485e2e8e
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-08 19:12:02 -0400

    Time: 15 ms (63.52%), Space: 7.7 MB (12.88%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -2,10 +2,10 @@ int removeDuplicates(int* nums, int numsSize){
     int insert = 1;
     int last = nums[0];
     
-    for (int j = 1; j < numsSize; j++) {
-        if (nums[j] != last) {
-            nums[insert] = nums[j];
-            last = nums[j];
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != last) {
+            nums[insert] = nums[i];
+            last = nums[i];
             insert++;
         }
     }
