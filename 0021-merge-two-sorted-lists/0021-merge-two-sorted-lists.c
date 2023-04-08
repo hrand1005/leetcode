1d10ad6c088caa36be81e8b5f3210ca41dd6fdd9
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-08 18:42:24 -0400

    Time: 7 ms (33.45%), Space: 6 MB (97.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -6,10 +6,10 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    if (list1 == NULL) {
+    if (!list1) {
         return list2;
     }
-    if (list2 == NULL) {
+    if (!list2) {
         return list1;
     }
     
