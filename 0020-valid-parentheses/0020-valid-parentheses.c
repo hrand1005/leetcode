1d2097441a685f291ce4a5ebcbb6266d58ad05d9
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-08 15:17:20 -0400

    Time: 0 ms (100.00%), Space: 5.5 MB (99.64%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,41 @@
+bool parens_match(char open, char close) {
+    bool is_match = false;
+    
+    if (open == '(' && close == ')' ||
+       open == '{' && close == '}' ||
+       open == '[' && close == ']') {
+        is_match = true;
+    }
+    return is_match;
+}
+
+bool isValid(char * s){
+    bool is_valid = true;
+    
+    const char* open = "({[";
+    const char* closed = ")}]";
+    
+    int idx = 0;
+    int s_len = strlen(s);
+    char* queue = malloc(sizeof(char) * (s_len+ 1));
+    
+    for (int i = 0; i < s_len; i++) {
+        if (strchr(open, s[i]) != NULL) {
+            queue[idx] = s[i];
+            idx++;
+        } else if (strchr(closed, s[i]) != NULL) {
+            if (idx == 0 || !parens_match(queue[idx-1], s[i])) {
+                is_valid = false;
+                break;
+            }
+            idx--;
+        }
+    }
+    
+    if (idx != 0) {
+        is_valid = false;
+    }
+                
+    free(queue);
+    return is_valid;
+}
