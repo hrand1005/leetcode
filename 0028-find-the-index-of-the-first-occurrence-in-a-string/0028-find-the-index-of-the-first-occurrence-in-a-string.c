5d0a3e398e288ce6c588f5fe9ec152c71fba785f
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-08 23:37:57 -0400

    Time: 4 ms (35.74%), Space: 5.8 MB (41.16%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -1,9 +1,8 @@
 int strStr(char * haystack, char * needle){
     char* start = strstr(haystack, needle);
     
-    if (!start) {
+    if (!start) 
         return -1;
-    } 
     
     int idx = 0;
     while (&haystack[idx] != start) 
