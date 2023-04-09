4f561c189d44431cd8161a321c403f12a2e87be5
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-09 12:45:17 -0400

    Time: 5 ms (36.83%), Space: 6.1 MB (33.80%) - LeetHub

--- 0066-plus-one/0066-plus-one.c
+++ 0066-plus-one/0066-plus-one.c
@@ -2,11 +2,12 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* plusOne(int* digits, int digitsSize, int* returnSize){
+    int digit;
     int carry = 1;
     int* sum = malloc(sizeof(int) * (digitsSize+1));
     
     for (int i = digitsSize-1; i >= 0; i--) {
-        int digit = digits[i] + carry;
+        digit = digits[i] + carry;
         if (digit > 9) {
             carry = 1;
             digit -= 10;
@@ -21,7 +22,7 @@ int* plusOne(int* digits, int digitsSize, int* returnSize){
         sum[0] = carry;
     } else {
         *returnSize = digitsSize;
-        // shift left
+        // shift all elements left
         for (int i = 1; i < digitsSize+1; i++) {
             sum[i-1] = sum[i];
         }
