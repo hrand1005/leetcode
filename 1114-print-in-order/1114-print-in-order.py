543d8e624eaa2bb4449e8e6345e3eb108503b707
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-04-21 21:48:58 -0400

    Time: 56 ms (50.13%), Space: 14.5 MB (6.33%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.py
+++ 1114-print-in-order/1114-print-in-order.py
@@ -7,19 +7,16 @@ class Foo:
 
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
-        # printFirst() outputs "first". Do not change or remove this line.
         printFirst()
         self.first_event.set()
 
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
         self.first_event.wait()
-        # printSecond() outputs "second". Do not change or remove this line.
         printSecond()
         self.second_event.set()
 
 
     def third(self, printThird: 'Callable[[], None]') -> None:
         self.second_event.wait()
-        # printThird() outputs "third". Do not change or remove this line.
         printThird()
