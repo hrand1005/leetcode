617ee2a01028cdbb85267c7ddfa33c19de5056b8
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-01 23:43:50 -0400

    Time: 37 ms (75.18%), Space: 13.7 MB (92.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,28 +1,19 @@
-"""
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        occurrences = {}
-        for char in s:
-            if not occurrences.get(char):
-                occurrences[char] = 1
-            else:    
-                occurrences[char] += 1
+class Solution(object):
+    def isAnagram(self, s, t):
+        occ = {}
+        for l in s:
+            if occ.get(l) is None:
+                occ[l] = 1
+            else:
+                occ[l] += 1
         
-        for char in t:
-            if occurrences.get(char, 0) == 0:
+        for l in t:
+            if occ.get(l, 0) == 0:
                 return False
-            occurrences[char] -= 1
-
-        for key, value in occurrences.items():
-            if value != 0:
+            occ[l] -= 1
+        
+        for v in occ.values():
+            if v != 0:
                 return False
-
+            
         return True    
-"""
-
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        s_list, t_list = list(s), list(t)
-        s_list.sort()
-        t_list.sort()
-        return s_list == t_list
