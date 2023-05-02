b176aebc993d22046fcd0100bf650ff8718185f8
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-02 19:07:59 -0400

    Time: 64 ms (15.02%), Space: 16.9 MB (6.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        occ = {}
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = {}
         for l in s:
-            if occ.get(l) is None:
-                occ[l] = 1
-            else:
-                occ[l] += 1
-        
+            count[l] = count.get(l, 0) + 1
         for l in t:
-            if occ.get(l, 0) == 0:
+            count[l] = count.get(l, 0) - 1
+        for v in count.values():
+            if v != 0:
                 return False
-            occ[l] -= 1
-            
-        return len(s) == len(t)
+        return True
+        
