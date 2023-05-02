ed690cd5befe6dd92bd5434e6d300d3dc3a46a78
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-02 19:50:01 -0400

    Time: 136 ms (10.27%), Space: 22 MB (5.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = {}
@@ -10,3 +11,15 @@ class Solution:
         l = list(s)
         l.sort()
         return "".join(l)
+"""
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for s in strs:
+            occ = [0] * 26
+            for l in s:
+                occ[ord(l)-ord("a")] += 1
+            key = tuple(occ)    
+            groups[key] = groups.get(key, []) + [s]
+        return groups.values()    
