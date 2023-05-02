152a5cfb098b5604a6cc16dc1847a82686793b82
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-02 19:13:03 -0400

    Time: 78 ms (44.83%), Space: 17.6 MB (5.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         comps = {}
         for i, n in enumerate(nums):
-            if comps.get(target-n) is not None:
-                return [comps.get(target-n), i]
+            if target-n in comps:
+                return [comps[target-n], i]
             comps[n] = i
         return []    
