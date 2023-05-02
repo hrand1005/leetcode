a98023ff56eeef2ad2e774c0c2fec1c627af2776
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-01 22:31:13 -0400

    Time: 78 ms (5.64%), Space: 16.4 MB (7.53%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
         neg = 0
@@ -8,3 +9,15 @@ class Solution:
                 neg += 1
         
         return 1 if neg % 2 == 0 else -1
+"""
+
+from functools import reduce
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        n = reduce(lambda x, y: x * y, nums)
+        if n == 0:
+            return 0
+        elif n > 0:
+            return 1
+        return -1
