57c45869cc8c3ca593424af5f9bf43f59afff18e
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-05 13:14:40 -0400

    Time: 238 ms (52.99%), Space: 23.7 MB (7.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,37 +1,13 @@
-"""
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        forward = [0] * len(nums)
-        forward[0] = nums[0]
+        products = [nums[0]]
         for i in range(1, len(nums)):
-            forward[i] = forward[i-1] * nums[i]
+            products.append(products[i-1] * nums[i])
         
-        backward = [0] * len(nums)
-        backward[-1] = nums[-1]
-        for i in range(len(nums)-2, -1, -1):
-            backward[i] = backward[i+1] * nums[i]
-        
-        result = [0] * len(nums)
-        result[0] = backward[1]
-        result[-1] = forward[-2]
-        for i in range(1, len(nums)-1):
-            result[i] = forward[i-1] * backward[i+1]
-        
-        return result
-"""
-
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [0] * len(nums)
-        product = 1
-        for i in range(0, len(nums)):
-            result[i] = product * nums[i]
-            product = result[i]
-        
-        product = 1
-        for i in range(len(nums)-1, 0, -1):
-            result[i] = product * result[i-1]
-            product *= nums[i]
-        result[0] = product
-        
-        return result
+        reverse = 1
+        products[-1] = products[-2]
+        for i in range(len(nums)-2, 0, -1):
+            reverse *= nums[i+1]
+            products[i] = products[i-1] * reverse
+        products[0] = reverse * nums[1]
+        return products
