d343437653504e07cc4bfe8fa998f5de069cebf8
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-05 13:00:00 -0400

    Time: 32 ms (21.03%), Space: 7.7 MB (53.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,46 +1,22 @@
-/*
 func productExceptSelf(nums []int) []int {
-    n := len(nums)
-    
-    forward := make([]int, n)
-    forward[0] = nums[0]
-    for i := 1; i < n; i++ {
-        forward[i] = forward[i-1] * nums[i]
+    forwards := make([]int, len(nums))
+    forwards[0] = nums[0]
+    for i := 1; i < len(forwards); i++ {
+        forwards[i] = nums[i] * forwards[i-1]
     }
     
-    backward := make([]int, n)
-    backward[n-1] = nums[n-1]
-    for i := n-2; i >= 0; i-- {
-        backward[i] = backward[i+1] * nums[i]
+    backwards := make([]int, len(nums))
+    backwards[len(backwards)-1] = nums[len(nums)-1]
+    for i := len(backwards)-2; i >= 0; i-- {
+        backwards[i] = nums[i] * backwards[i+1]
     }
     
-    result := make([]int, n)
-    result[0] = backward[1]
-    result[n-1] = forward[n-2]
-    for i := 1; i < n-1; i++ {
-        result[i] = forward[i-1] * backward[i+1]
+    result := make([]int, len(nums))
+    for i := 1; i < len(result)-1; i++ {
+        result[i] = forwards[i-1] * backwards[i+1]
     }
-    
-    return result
-}
-*/
-
-func productExceptSelf(nums []int) []int {
-    n := len(nums)
-    result := make([]int, n)
-    
-    product := 1
-    for i := 0; i < n; i++ {
-        result[i] = product * nums[i]
-        product *= nums[i]
-    }
-    
-    product = 1
-    for i := n-1; i > 0; i-- {
-        result[i] = product * result[i-1]
-        product *= nums[i]
-    }
-    result[0] = product
+    result[0] = backwards[1]
+    result[len(result)-1] = forwards[len(forwards)-2]
     
     return result
 }
