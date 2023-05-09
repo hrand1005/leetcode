fc8b35f810a8cf6ff4c3c5b066a7c00b9d1f224c
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-09 19:21:37 -0400

    Time: 2 ms (63.35%), Space: 2 MB (98.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.go
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -1,17 +1,17 @@
 func isValid(s string) bool {
-    openToClose := map[rune]rune{
-        '{': '}',
-        '(': ')',
-        '[': ']',
+    match := map[rune]rune{
+        ')': '(',
+        ']': '[',
+        '}': '{',
     }
-    
     stack := make([]rune, 0, len(s))
     for _, c := range s {
-        if _, ok := openToClose[c]; ok {
+        if _, ok := match[c]; !ok {
             stack = append(stack, c)
-        } else if len(stack) == 0 || openToClose[stack[len(stack)-1]] != c  {
-            return false
         } else {
+            if len(stack) == 0 || stack[len(stack)-1] != match[c] {
+                return false
+            }
             stack = stack[:len(stack)-1]
         }
     }
