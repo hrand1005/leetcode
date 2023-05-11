2dccec421e745d72c44394fba482bf50cb56c5ed
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-10 20:29:46 -0400

    Time: 63 ms (63.30%), Space: 20.4 MB (9.61%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,79 +1,29 @@
-"""
 class MinStack:
-
     def __init__(self):
-        self.min_val = 0
         self.stack = []
+        self.min_stack = []
 
     def push(self, val: int) -> None:
-        if len(self.stack) == 0 or val < self.min_val:
-            self.min_val = val
-        self.stack.append(val)    
-
-    def pop(self) -> None:
-        val = self.stack.pop()
-        if val == self.min_val:
-            self._set_next_min()
-
-    def top(self) -> int:
-        return self.stack[-1]
-        
-    def getMin(self) -> int:
-        return self.min_val
-    
-    def _set_next_min(self) -> int:
-        if len(self.stack) != 0:
-            self.min_val = self.stack[0]
-            for n in self.stack:
-                self.min_val = min(self.min_val, n)
-
-class MinStack:
-
-    def __init__(self):
-        # stack of (min, cur) tuples
-        self.stack = []
-
-    def push(self, val: int) -> None:
-        if len(self.stack) == 0:
-            self.stack.append((val, val))
+        self.stack.append(val)
+        if self.min_stack:
+            self.min_stack.append(min(val, self.min_stack[-1]))
         else:
-            prev_min = self.stack[-1][0]
-            new_min = min(prev_min, val)
-            self.stack.append((new_min, val))
+            self.min_stack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1][1]
-        
+        return self.stack[-1]
+
     def getMin(self) -> int:
-        return self.stack[-1][0]
-"""
-class Node:
-    def __init__(self, val=0, min_val=0, prev=None):
-        self.val = val
-        self.min_val = min_val
-        self.prev = prev
+        return self.min_stack[-1]
 
-class MinStack:
 
-    def __init__(self):
-        self.cur = None
-
-    def push(self, val: int) -> None:
-        if self.cur == None:
-            self.cur = Node(val=val, min_val=val)
-        else:
-            prev_min = self.cur.min_val
-            new_min = min(prev_min, val)
-            self.cur = Node(val=val, min_val=new_min, prev=self.cur)
-
-    def pop(self) -> None:
-        self.cur = self.cur.prev
-
-    def top(self) -> int:
-        return self.cur.val
-        
-    def getMin(self) -> int:
-        return self.cur.min_val
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
