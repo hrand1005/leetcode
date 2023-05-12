50247fad00fa17b1b208cc3bf791ba7db5a3d4b4
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-05-11 21:55:14 -0400

    Time: 196 ms (5.09%), Space: 16.8 MB (13.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,50 +1,19 @@
-OPERATORS = ["+", "-", "*", "/"]
-"""
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        for i in range(len(tokens)):
-            if tokens[i] not in OPERATORS:
-                tokens[i] = int(tokens[i])
-                
-        while len(tokens) > 1:
-            index = self.find_next_operator(tokens)
-            result = self.simplify(tokens[index-2], tokens[index-1], tokens[index])
-            tokens = tokens[:index-2] + [result] + tokens[index+1:]
-            
-        return tokens[0]
-        
-    def find_next_operator(self, tokens: List[str]) -> int:    
-        for i in range(len(tokens)):
-            if tokens[i] in OPERATORS:
-                return i
-        return -1   
-    
-    def simplify(self, x: int, y: int, op: str) -> str:
-        if op == "+":
-            return x + y
-        if op == "-":
-            return x - y
-        if op == "*":
-            return x * y
-        if op == "/":
-            return int(float(x)/y)
-"""
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for t in tokens:
-            if t in OPERATORS:
-                y, x = stack.pop(), stack.pop()
-                if t == "+":
-                    stack.append(x + y)
-                if t == "-":
-                    stack.append(x - y)
-                if t == "*":
-                    stack.append(x * y)
-                if t == "/":
-                    stack.append(int(x / y))
+            if t == "+":
+                a, b = stack[-1], stack[-2]
+                stack = stack[:-2] + [a+b]
+            elif t == "-":
+                a, b = stack[-1], stack[-2]
+                stack = stack[:-2] + [b-a]
+            elif t == "*":
+                a, b = stack[-1], stack[-2]
+                stack = stack[:-2] + [a*b]
+            elif t == "/":
+                a, b = stack[-1], stack[-2]
+                stack = stack[:-2] + [int(b/a)]
             else:
                 stack.append(int(t))
-        
-        return stack[0]
+        return stack[-1]
