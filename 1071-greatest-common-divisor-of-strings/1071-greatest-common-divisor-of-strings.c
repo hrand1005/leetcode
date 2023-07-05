1a891de3d91039e221d6c98fc3257a9de44eaaf6
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-04 20:37:31 -0400

    Time: 0 ms (100.00%), Space: 6.2 MB (7.48%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.c
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.c
@@ -9,16 +9,15 @@ int gcd(int a, int b) {
 }
 
 char * gcdOfStrings(char * str1, char * str2){
-    size_t n;
     char *result;
+    
+    size_t n;
     size_t s1_len = strlen(str1);
     size_t s2_len = strlen(str2);
     size_t concat_len = s1_len + s2_len + 1;
-    char s1s2[concat_len];
-    char s2s1[concat_len];
     
-    memset(s1s2, '\0', concat_len);
-    memset(s2s1, '\0', concat_len);
+    char *s1s2 = calloc(sizeof(char), concat_len);
+    char *s2s1 = calloc(sizeof(char), concat_len);
     
     strncpy(s1s2, str1, s1_len);
     strncat(s1s2, str2, s2_len);
@@ -27,11 +26,15 @@ char * gcdOfStrings(char * str1, char * str2){
     strncat(s2s1, str1, s1_len);
     
     if (strncmp(s1s2, s2s1, s1_len + s2_len) != 0)
-        return calloc(sizeof(char), 1);
-    
-    n = gcd(s1_len, s2_len);
-    result = malloc(sizeof(char) * n + 1);
-    result[n] = '\0';
-    strncpy(result, str1, n);
+        result = calloc(sizeof(char), 1);
+    else {
+        n = gcd(s1_len, s2_len);
+        result = malloc(sizeof(char) * n + 1);
+        result[n] = '\0';
+        strncpy(result, str1, n);
+    }
+        
+    free(s1s2);
+    free(s2s1);
     return result;
 }
