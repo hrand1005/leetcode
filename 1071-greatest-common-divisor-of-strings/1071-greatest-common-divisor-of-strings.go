594c5c6ae14641aa3166e3661152b4963349be29
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-04 20:12:15 -0400

    Time: 0 ms (100.00%), Space: 2.5 MB (64.14%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
@@ -1,4 +1,20 @@
 func gcdOfStrings(str1 string, str2 string) string {
+    if str1 + str2 != str2 + str1 {
+        return ""
+    }
+    gcd := calculateGCD(len(str1), len(str2))
+    return str1[:gcd]
+}
+
+func calculateGCD(a, b int) int {
+    for b != 0 {
+        a, b = b, a % b
+    }
+    return a
+}
+
+/*
+func gcdOfStrings(str1 string, str2 string) string {
     i := 0
     sub := ""
     maxSub := ""
@@ -24,3 +40,4 @@ func min(a, b int) int {
     }
     return b
 }
+*/
