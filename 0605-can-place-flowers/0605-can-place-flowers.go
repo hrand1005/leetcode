7e7d39d06448b9e6837b632adb6087f67d471e31
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-05 15:48:29 -0400

    Time: 18 ms (54.45%), Space: 7 MB (7.86%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.go
+++ 0605-can-place-flowers/0605-can-place-flowers.go
@@ -1,3 +1,4 @@
+/*
 func canPlaceFlowers(flowerbed []int, n int) bool {
     capacity := 0
     pos := 0
@@ -12,4 +13,29 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
         }
     }
     return n <= capacity
+}
+*/
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    return maxPlantable(flowerbed) >= n
+}
+
+func maxPlantable(flowerbed[]int) int {
+    if len(flowerbed) == 0 {
+        return 0
+    }
+    if len(flowerbed) == 1 {
+        if flowerbed[0] == 0 {
+            return 1
+        }
+        return 0
+    }
+    
+    if flowerbed[0] == 0 && flowerbed[1] == 0 {
+        return maxPlantable(flowerbed[2:]) + 1
+    }
+    if flowerbed[0] == 0 {
+        return maxPlantable(flowerbed[1:])
+    }
+    return maxPlantable(flowerbed[2:])
 }
