0ff8a45624c7158c1daebd02e5b3168e69a072ef
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-05 16:04:19 -0400

    Time: 23 ms (48.53%), Space: 7.2 MB (38.66%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.c
+++ 0605-can-place-flowers/0605-can-place-flowers.c
@@ -1,3 +1,4 @@
+/*
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
     size_t cap = 0;
     size_t pos = 0;
@@ -11,4 +12,19 @@ bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
             pos++;
     }
     return (cap >= n);
+}
+*/
+
+int placeable_flowers(int* flowerbed, int flowerbed_size) {
+    if (flowerbed_size == 0) 
+        return 0;
+    if (flowerbed_size == 1)
+        return flowerbed[0] == 0;
+    int placeable = (flowerbed[0] == 0 && flowerbed[1] == 0);
+    int increment = (placeable || flowerbed[0] == 1) ? 2 : 1;
+    return placeable_flowers(&flowerbed[increment], flowerbed_size - increment) + placeable;
+}
+
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
+    return (placeable_flowers(flowerbed, flowerbedSize) >= n);
 }
