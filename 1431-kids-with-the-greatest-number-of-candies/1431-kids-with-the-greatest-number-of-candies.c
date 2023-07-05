fdb2c5df06a657b05d72296dc31435637b08f189
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-04 23:27:06 -0400

    Time: 4 ms (52.63%), Space: 6.1 MB (72.63%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.c
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.c
@@ -2,13 +2,13 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize){
-    int max_candies = 0; 
+    int maxc = 0; 
     for (int i = 0; i < candiesSize; i++)
-        max_candies = (max_candies > candies[i]) ? max_candies : candies[i];
+        maxc = (maxc > candies[i]) ? maxc : candies[i];
     
     bool *result = malloc(sizeof(bool) * candiesSize);
     for (int i = 0; i < candiesSize; i++)
-        result[i] = (candies[i] + extraCandies) >= max_candies;
+        result[i] = (candies[i] + extraCandies) >= maxc;
     
     *returnSize = candiesSize;
     return result;
