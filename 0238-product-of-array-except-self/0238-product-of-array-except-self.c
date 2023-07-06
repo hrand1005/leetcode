a8723b9c79f5329e08392892e49923e37d0c3c64
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-06 11:43:08 -0400

    Time: 120 ms (8.96%), Space: 18.7 MB (73.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.c
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+/*
 int* productExceptSelf(int* nums, int numsSize, int* returnSize){
     int product, i;
     int forwards[numsSize];
@@ -21,6 +22,28 @@ int* productExceptSelf(int* nums, int numsSize, int* returnSize){
     result[numsSize-1] = forwards[numsSize-2];
     for (i = 1; i < numsSize-1; i++)
         result[i] = forwards[i-1] * backwards[i+1];
+    
+    *returnSize = numsSize;
+    return result;
+}
+*/
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize){
+    int product, i;
+    int backwards;
+    int *result = malloc(sizeof(int) * numsSize);
+    
+    result[0] = nums[0];
+    for (i = 1; i < numsSize; i++) {
+        result[i] = result[i-1] * nums[i];
+    }
+    
+    backwards = 1;
+    for (i = numsSize-1; i > 0; i--) {
+        result[i] = result[i-1] * backwards;
+        backwards *= nums[i];
+    }
+    result[0] = backwards;
     
     *returnSize = numsSize;
     return result;
