0850da86773ff7b44a1935e4bf356d2468d13348
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-05 21:29:50 -0400

    Time: 7 ms (49.53%), Space: 6.5 MB (89.77%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.c
@@ -0,0 +1,23 @@
+char * reverseVowels(char * s){
+    int left, right;
+    char tmp;
+    const char *vowels = "aeiouAEIOU";
+    
+    left = 0;
+    right = strlen(s)-1;
+    
+    while (left < right) {
+        while (left < right && !strchr(vowels, s[left]))
+            left++;
+        while (right > left && !strchr(vowels, s[right]))
+            right--;
+        if (strchr(vowels, s[left]) && strchr(vowels, s[right])) {
+            tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+        }
+        left++;
+        right--;
+    }
+    return s;
+}
