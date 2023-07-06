8a1f98547096ada5b4a0dfd5cf2e78bc9d9c010b
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-05 21:22:32 -0400

    Time: 12 ms (20.96%), Space: 6.3 MB (23.62%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.go
@@ -0,0 +1,24 @@
+import (
+    "strings"
+)
+
+func reverseVowels(s string) string {
+    const vowels = "aeiouAEIOU"
+    sSlice := strings.Split(s, "")
+    
+    left, right := 0, len(sSlice)-1
+    for left < right {
+        for left < right && !strings.Contains(vowels, sSlice[left]) {
+            left++
+        }
+        for right > left && !strings.Contains(vowels, sSlice[right]) {
+            right--
+        }
+        if strings.Contains(vowels, sSlice[left]) && strings.Contains(vowels, sSlice[right]) {
+            sSlice[left], sSlice[right] = sSlice[right], sSlice[left]
+        }
+        left++
+        right--
+    } 
+    return strings.Join(sSlice, "")
+}
