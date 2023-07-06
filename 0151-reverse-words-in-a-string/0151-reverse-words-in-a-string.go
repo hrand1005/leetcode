fb90cfae59157261827168dfbc02dc7d9a870bae
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-05 22:18:34 -0400

    Time: 0 ms (100.00%), Space: 2.9 MB (92.11%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.go
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.go
@@ -1,12 +1,5 @@
 func reverseWords(s string) string {
-    raw := strings.Split(s, " ")
-    
-    words := make([]string, 0, len(raw))
-    for _, w := range raw {
-        if w != "" {
-            words = append(words, w)
-        }
-    }
+    words := strings.Fields(s)
     
     for i := 0; i < len(words)/2; i++ {
         words[i], words[len(words)-1-i] = words[len(words)-1-i], words[i]
