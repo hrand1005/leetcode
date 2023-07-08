f3b6e32fd84693ee6fde98e7af4227430dbc994d
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-08 14:37:15 -0400

    Time: 73 ms (5.49%), Space: 7.9 MB (9.58%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.go
@@ -0,0 +1,26 @@
+func longestSubarray(nums []int) int {
+    zeroFound := false
+    before, after, maxSeq := 0, 0, 0
+    for _, n := range nums {
+        if n == 0 {
+            zeroFound = true
+            maxSeq = max(maxSeq, before + after)
+            before = after
+            after = 0
+        } else {
+            after++
+        }
+    }
+    maxSeq = max(maxSeq, before + after)
+    if zeroFound {
+        return maxSeq
+    }
+    return maxSeq-1
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
