98cc26f38e36dfd96b6933b9438399a005dba00e
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-08 14:53:50 -0400

    Time: 70 ms (7.06%), Space: 10.6 MB (5.96%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.c
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.c
@@ -1,3 +1,4 @@
+/*
 int longestSubarray(int* nums, int nums_size) {
     size_t l, r, zc;
     l = r = zc = 0;
@@ -7,4 +8,24 @@ int longestSubarray(int* nums, int nums_size) {
             zc--;
     }
     return r - l - 1;
+}
+*/
+
+int longestSubarray(int* nums, int nums_size) {
+    size_t before, after, seq, max_seq, zero_found;
+    before = after = seq = max_seq = zero_found = 0;
+    
+    for (size_t i = 0; i < nums_size; i++) {
+        if (nums[i] == 0) {
+            zero_found = 1;
+            seq = before + after;
+            max_seq = (max_seq >= seq) ? max_seq : seq;
+            before = after;
+            after = 0;
+        } else
+            after++;
+    }
+    seq = before + after;
+    max_seq = (max_seq >= seq) ? max_seq : seq;
+    return (zero_found) ? max_seq : max_seq - 1;
 }
