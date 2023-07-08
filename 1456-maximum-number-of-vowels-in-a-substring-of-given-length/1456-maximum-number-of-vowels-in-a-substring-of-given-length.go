fdd91893fa739fcf8e85d76c8f984b1a34df2d4c
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-08 11:03:53 -0400

    Time: 19 ms (38.99%), Space: 4.7 MB (63.99%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.go
@@ -0,0 +1,35 @@
+func maxVowels(s string, k int) int {
+    vowelCount := 0
+    for i := 0; i < k; i++ {
+        if isVowel(string(s[i])) {
+            vowelCount++
+        }
+    }
+    maxVowels := vowelCount
+    
+    l, r := 0, k
+    for r < len(s) {
+        if isVowel(string(s[l])) {
+            vowelCount--
+        }
+        if isVowel(string(s[r])) {
+            vowelCount++
+        }
+        l++
+        r++
+        maxVowels = max(maxVowels, vowelCount)
+    }
+    return maxVowels
+}
+
+func isVowel(s string) bool {
+    vowels := "aeiou"
+    return strings.Contains(vowels, s)
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
