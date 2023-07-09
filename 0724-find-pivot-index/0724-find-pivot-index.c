06f63652d404825cb4af0dc3c8a9d205ad3a32d8
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-08 23:32:47 -0400

    Time: 29 ms (39.78%), Space: 6.6 MB (34.77%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.c
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -1,9 +1,9 @@
 int pivotIndex(int* nums, int nums_size){
-    int total = 0;
+    int total, acc, i;
+    total = acc = 0;
     for (int i = 0; i < nums_size; i++)
         total += nums[i];
     
-    int acc = 0;
     for (int i = 0; i < nums_size; i++) {
         if (total - nums[i] - acc == acc)
             return i;
