28370318b6463b875bb61c34914a36e012d2a892
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-08 23:20:29 -0400

    Time: 0 ms (100.00%), Space: 5.9 MB (35.44%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.c
@@ -0,0 +1,9 @@
+int largestAltitude(int* gain, int gain_size){
+    int h, hmax;
+    h = hmax = 0;
+    for (int i = 0; i < gain_size; i++) {
+        h += gain[i];
+        hmax = (hmax >= h) ? hmax : h;
+    }
+    return hmax;
+}
