aff2167500d4bfdad15623dd92057a01e6c47f57
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-08 23:48:10 -0400

    Time: 33 ms (64.69%), Space: 7.4 MB (51.42%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
@@ -6,22 +6,19 @@ func findDifference(nums1 []int, nums2 []int) [][]int {
     
     map2 := make(map[int]bool)
     for i := 0; i < len(nums2); i++ {
-        if _, ok := map1[nums2[i]]; ok {
-            map1[nums2[i]] = false
-        } else {
-            map2[nums2[i]] = true
-        }
+        map2[nums2[i]] = true
     }
     
     unique1 := make([]int, 0, len(nums1))
-    for k, v := range map1 {
-        if v {
+    for k, _ := range map1 {
+        if _, ok := map2[k]; !ok {
             unique1 = append(unique1, k)
         }
     }
+    
     unique2 := make([]int, 0, len(nums2))
-    for k, v := range map2 {
-        if v {
+    for k, _ := range map2 {
+        if _, ok := map1[k]; !ok {
             unique2 = append(unique2, k)
         }
     }
