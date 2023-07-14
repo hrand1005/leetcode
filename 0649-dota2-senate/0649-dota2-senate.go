d8a55920596a35706c84298201dd20d3ffecfd51
Herbie Rand <53189515+hrand1005@users.noreply.github.com>
2023-07-14 19:30:45 -0400

    Time: 86 ms (5.74%), Space: 9.2 MB (5.74%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.go
@@ -0,0 +1,34 @@
+func predictPartyVictory(senate string) string {
+    partyCount := make(map[rune]int)
+    for _, p := range senate {
+        partyCount[p]++
+    }
+    
+
+    rVote, dVote := 0, 0
+    for partyCount['R'] != 0 && partyCount['D'] != 0 {
+        if senate[0] == 'R' {
+            if dVote == 0 {
+                rVote++
+                senate = senate[1:] + "R"
+            } else {
+                dVote--
+                partyCount['R']--
+                senate = senate[1:]
+            }
+        } else {
+            if rVote == 0 {
+                dVote++
+                senate = senate[1:] + "D"
+            } else {
+                rVote--
+                partyCount['D']--
+                senate = senate[1:]
+            }
+        }
+    }
+    if senate[0] == 'R' {
+        return "Radiant"
+    }
+    return "Dire"
+}
